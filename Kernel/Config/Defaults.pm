7f76ac7f25b93125a6b2a42dd0de79ff2b3b341a
Roy Kaldung <rk@znuny.com>
2021-02-12 09:23:28 +0100

    Replaced URL for Package::RepositoryRoot.

--- Kernel/Config/Defaults.pm
+++ Kernel/Config/Defaults.pm
@@ -1,5 +1,6 @@
 # --
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
+# Copyright (C) 2021 Znuny GmbH, https://znuny.org/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -1083,7 +1084,7 @@ sub LoadDefaults {
     # Package::RepositoryRoot
     # (get online repository list, use the fist availabe result)
     $Self->{'Package::RepositoryRoot'} = [
-        'https://ftp.otrs.org/pub/otrs/misc/packages/repository.xml',
+        'https://download.znuny.org/releases/misc/packages/repository.xml',
     ];
 
     # Package::RepositoryList
