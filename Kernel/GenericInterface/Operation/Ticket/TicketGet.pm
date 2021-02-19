3713f92d7fc36568f13975a6a3ff6301ebf4257f
reneeb <info@perl-services.de>
2021-02-19 09:17:17 +0100

    fix [#16]: GenericInterface - ticket with less articles then the given article limit has effect on subsequent ticket
    Given ticket 1 has 7 articles and ticket 2 has 12 articles and an article limit is set (e.g. to 100). When
    a webservice endpoint is called to get tickets 1 and 2, only 7 articles are shown. This fix checks the
    article limit and the articles in the ticket for each ticket.

--- Kernel/GenericInterface/Operation/Ticket/TicketGet.pm
+++ Kernel/GenericInterface/Operation/Ticket/TicketGet.pm
@@ -340,6 +340,7 @@ sub Run {
     # start ticket loop
     TICKET:
     for my $TicketID (@TicketIDs) {
+        my $TicketArticleLimit = $ArticleLimit;
 
         my $TicketObject = $Kernel::OM->Get('Kernel::System::Ticket');
 
@@ -428,15 +429,15 @@ sub Run {
 
         # Modify ArticleLimit if it is greater then number of articles (see bug#14585).
         if ( $ArticleLimit > scalar @Articles ) {
-            $ArticleLimit = scalar @Articles;
+            $TicketArticleLimit = scalar @Articles;
         }
 
         # Set number of articles by ArticleLimit and ArticleOrder parameters.
-        if ( IsArrayRefWithData( \@Articles ) && $ArticleLimit ) {
+        if ( IsArrayRefWithData( \@Articles ) && $TicketArticleLimit ) {
             if ( $ArticleOrder eq 'DESC' ) {
                 @Articles = reverse @Articles;
             }
-            @Articles = @Articles[ 0 .. ( $ArticleLimit - 1 ) ];
+            @Articles = @Articles[ 0 .. ( $TicketArticleLimit - 1 ) ];
         }
 
         # start article loop
