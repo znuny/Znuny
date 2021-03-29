b5c2220e972c832d8c9a8f08ccb4f8418bb8bc13
reneeb <info@perl-services.de>
2021-03-29 10:31:54 +0200

    update libwww-perl (LWP::UserAgent et al)

--- Kernel/cpan-lib/LWP/RobotUA.pm
+++ Kernel/cpan-lib/LWP/RobotUA.pm
@@ -2,7 +2,7 @@ package LWP::RobotUA;
 
 use base qw(LWP::UserAgent);
 
-our $VERSION = '6.26';
+our $VERSION = '6.53';
 
 require WWW::RobotRules;
 require HTTP::Request;
--- Kernel/cpan-lib/LWP/Simple.pm
+++ Kernel/cpan-lib/LWP/Simple.pm
@@ -2,7 +2,7 @@ package LWP::Simple;
 
 use strict;
 
-our $VERSION = '6.26';
+our $VERSION = '6.53';
 
 require Exporter;
 
