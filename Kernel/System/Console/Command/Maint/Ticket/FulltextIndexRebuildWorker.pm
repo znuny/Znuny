91ed7b360ce7961f821114e4ee290e373adf9f64
Pawel Boguslawski <pawel.boguslawski@ib.pl>
2021-04-19 20:43:47 +0200

    Missing dependency added
    Fixes: d4abf57a6d6e21fdb05c4c6fd30498bca0f4f7df
    Author-Change-Id: IB#1046574

--- Kernel/System/Console/Command/Maint/Ticket/FulltextIndexRebuildWorker.pm
+++ Kernel/System/Console/Command/Maint/Ticket/FulltextIndexRebuildWorker.pm
@@ -1,6 +1,7 @@
 # --
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 # Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+# Copyright (C) 2014-2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -22,6 +23,7 @@ use Kernel::System::VariableCheck qw(:all);
 
 our @ObjectDependencies = (
     'Kernel::Config',
+    'Kernel::System::Cache::Memcached',
     'Kernel::System::DB',
     'Kernel::System::Log',
     'Kernel::System::PID',
