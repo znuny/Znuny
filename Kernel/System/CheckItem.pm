74b833e159cc936c3ad7afe625c020e6bd8a7606
Pawel Boguslawski <pawel.boguslawski@ib.pl>
2021-05-07 12:14:24 +0200

    Configurable resolver timeout
    OTRS uses hardcoded 3s timeout for DNS queries while checking MX
    (if CheckMXRecord is enabled). 3s may be too short in some scenarios
    (sporadic errors in MX validation of remote addresses). DNS timeout
    should be configurable to allow admin to fine-tune this setting.
    
    This mod introduces new SysConfig parameter ResolverTimeout that
    allows one to set custom timeout for DNS queries fired by OTRS
    (now only for CheckMXRecord algo).
    
    Author-Change-Id: IB#1058919

--- Kernel/System/CheckItem.pm
+++ Kernel/System/CheckItem.pm
@@ -1,6 +1,7 @@
 # --
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 # Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+# Copyright (C) 2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -147,8 +148,9 @@ sub CheckEmail {
         if ($Resolver) {
 
             # it's no fun to have this hanging in the web interface
-            $Resolver->tcp_timeout(3);
-            $Resolver->udp_timeout(3);
+            my $ResolverTimeout = $ConfigObject->Get('ResolverTimeout') // 3;
+            $Resolver->tcp_timeout($ResolverTimeout);
+            $Resolver->udp_timeout($ResolverTimeout);
 
             # check if we need to use a specific name server
             my $Nameserver = $ConfigObject->Get('CheckMXRecord::Nameserver');
