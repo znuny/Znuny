znuny/Znuny

commit a972be844d55d8cd91f0934cfc0dbafdeb1f059b

Filename trimming fixed and unit tests adjusted
Filename trimming algo changed to simiar to original
because wrong assumtion about utf-8 char size (may
be greater than 2 bytes). Unit tests adjusted.

Fixes: 62e205052ee01d6a4195d572ba2f1a0ea9573597
Author-Change-Id: IB#1016067

Kernel/System/Main.pm
@@ -214,25 +214,43 @@ sub FilenameCleanUp {
         # Enclosed alphanumerics are kept on older Perl versions, make sure to replace them too.
         $Param{Filename} =~ s/[\x{2460}-\x{24FF}]/_/g;
 
-        # Trim filename to 77 chars if longer than 110 chars (=220 bytes in case of all UTF-8 chars)
-        # to avoid exceeding filesystem limits (i.e. 255 byte filename length limit in XFS) and to
-        # avoid double trimming after adding md5 suffix (33 chars) below; must leave enough space
-        # for extra application extensions (i.e. .content_type).
-        if ( length($Param{Filename}) > 110 ) {
+        # If filename occupies > 220b, trim it to valid UTF-8 string occupying no more
+        # than 187 bytes; final length with md5 part (33b) will be <= 220b; this leaves enough
+        # room for extra extensions like .content_type; total file length must not exceed
+        # 255b (many filesystems limit).
+        # This algo must avoid filename changes if FilenameCleanUp() is called more than once
+        # for the same filename variable.
 
-            # If file extension exists and is not longer than 30 chars
-            # than leave whole extension and trim filename part before
-            # extension only; trim whole filename otherwise.
+        if ( length encode( 'UTF-8', $Param{Filename} ) > 220 ) {
+
+            # Separate filename and extension.
 
             my $FileName = $Param{Filename};
-            my $FileExt = '';
+            my $FileExt  = '';
             if ( $Param{Filename} =~ /(.*)\.+([^.]+)$/ ) {
-                if ( length($2) <= 30 ) {
-                    $FileName = $1;
-                    $FileExt = '.' . $2;
-                }
+                $FileName = $1;
+                $FileExt  = '.' . $2;
             }
-            $Param{Filename} = substr($FileName, 0, 77 - length($FileExt)) . $FileExt;
+
+            my $ModifiedName = $FileName . $FileExt;
+            while ( length encode( 'UTF-8', $ModifiedName ) > 187 ) {
+
+                # Remove character by character starting from the end of the filename string
+                # until we get acceptable 187 byte long filename size including extension.
+                if ( length $FileName > 10 ) {
+                    chop $FileName;
+                }
+
+                # If we reached minimum filename length (10 chars), remove characters from
+                # the end of the extension string.
+                else {
+                    chop $FileExt;
+                }
+
+                $ModifiedName = $FileName . $FileExt;
+            }
+
+            $Param{Filename} = $ModifiedName;
         }
 
         # If filename was modified, add unique filename suffix (MD5 of original filename)
@@ -240,8 +258,8 @@ sub FilenameCleanUp {
         # result for different original filenames (i.e. in the same upload
         # form or in the same incoming e-mail article) which may cause problems.
         if ( $Param{Filename} ne $FilenameOrig ) {
-            my $FileName = $Param{Filename};
-            my $FileExt = '';
+            $FileName = $Param{Filename};
+            $FileExt = '';
             if ( $Param{Filename} =~ /(.*)\.+([^.]+)$/ ) {
                 $FileName = $1;
                 $FileExt = '.' . $2;

scripts/test/EmailParser.t
@@ -1,6 +1,7 @@
 # --
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 # Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+# Copyright (C) 2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -602,22 +603,22 @@ $Self->Is(
 );
 $Self->Is(
     $Attachments[5]->{Filename} || '',
-    '報告書_..txt',
+    '報告書_._a86cfd50111b38859f9dabbd60b4e11e.txt',
     "#12 Filename check",
 );
 $Self->Is(
     $Attachments[6]->{Filename} || '',
-    '金田_美羽',
+    '金田_美羽_0fd37459af13fce03ba3ce6ac1fb6758',
     "#12 Filename check",
 );
 $Self->Is(
     $Attachments[7]->{Filename} || '',
-    '國科會50科學之旅活動計畫徵求書_r_final_.doc',
+    '國科會50科學之旅活動計畫徵求書_r_final_ab9ef92ceaa8fdf051527dfd01ca800f.doc',
     "#12 Filename check",
 );
 $Self->Is(
     $Attachments[8]->{Filename} || '',
-    '2차_보도자료.hwp',
+    '2차 보도자료.hwp',
     "#12 Filename check",
 );
 $Self->True(

scripts/test/EmailParser/DuplicateFilenameSpecialCharacters.t
@@ -1,6 +1,7 @@
 # --
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 # Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+# Copyright (C) 2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -37,13 +38,13 @@ $Self->Is(
 
 $Self->Is(
     $Attachments[1]->{Filename} || '',
-    '_Terminology_Guide_.pdf',
+    '_Terminology_Guide_3d444c64edc7f4eeaa611215329c7b08.pdf',
     "First attachment",
 );
 
 $Self->Is(
     $Attachments[2]->{Filename} || '',
-    '_Terminology_Guide_.pdf',
+    '_Terminology_Guide_3d444c64edc7f4eeaa611215329c7b08.pdf',
     "First attachment",
 );
 

scripts/test/EmailParser/EmptyEmail.t
@@ -1,6 +1,7 @@
 # --
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 # Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+# Copyright (C) 2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -51,7 +52,7 @@ $Self->Is(
 
 $Self->Is(
     $Attachments[1]->{Filename},
-    'Łatwa_sprawa.txt',
+    'Łatwa sprawa.txt',
     "Empty attachment name",
 );
 

scripts/test/EmailParser/FilenameWithCRLF.t
@@ -1,6 +1,7 @@
 # --
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 # Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+# Copyright (C) 2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -46,7 +47,7 @@ $Self->Is(
 # LF => 0A hexadecimal
 $Self->Is(
     $Attachments[2]->{'Filename'} || '',
-    'Test__test_test_test_dokument.eml',
+    'Test  test test test dokument_72f42c7d9d3d7deda64e7f3bb68c2cc0.eml',
     "Filename with multiple newlines removed",
 );
 

scripts/test/EmailParser/FilenameWithNewline.t
@@ -44,7 +44,7 @@ $Self->Is(
 
 $Self->Is(
     $Attachments[2]->{'Filename'} || '',
-    'Test testtestt 1231234_34_Testtesttes testes testtesttestt - testtesttes _8c51a4c1d1ca2b12bb4efabe82f41428.eml',
+    'Test testtestt 1231234_34_Testtesttes testes testtesttestt - testtesttes dokumentów_Sprzedaż_ testTE _..._ _TE#123123123_8c51a4c1d1ca2b12bb4efabe82f41428.eml',
     "Filename with multiple newlines removed",
 );
 

scripts/test/EmailParser/NestedMessage.t
@@ -40,7 +40,7 @@ $Self->Is(
 
 $Self->Is(
     $Attachments[2]->{Filename} || '',
-    '_Presse-Greenpeace_ Morgen wird Greenpeace-Einspruch gegenEmbryonen-Paten_962fb2b3711f7f233945657da34d17da.eml',
+    '_Presse-Greenpeace_ Morgen wird Greenpeace-Einspruch gegenEmbryonen-Patent verhandelt - NeueDokumentation ueber Patente auf Leben_962fb2b3711f7f233945657da34d17da.eml',
     "Nested message filename",
 );
 

scripts/test/Main.t
@@ -52,32 +52,32 @@ my @Tests = (
     {
         Name         => 'FilenameCleanUp() - Long filename no extension - Name',
         FilenameOrig => 'a' x 250,
-        FilenameNew  => 'a' x 77 . '_1bdbdf1c9087c796394bcda5789f7206',
+        FilenameNew  => 'a' x 187 . '_1bdbdf1c9087c796394bcda5789f7206',
     },
     {
         Name         => 'FilenameCleanUp() - Long filename short extension - Name',
         FilenameOrig => 'a' x 250 . '.test',
-        FilenameNew  => 'a' x 72 . '_934489bed8c5675451bf4dda19be2a01.test',
+        FilenameNew  => 'a' x 182 . '_934489bed8c5675451bf4dda19be2a01.test',
     },
     {
         Name         => 'FilenameCleanUp() - Short filename long extension - Name',
         FilenameOrig => 'test.' . 'a' x 250,
-        FilenameNew  => 'test_5f2a12b7a37e955f02cd994884d4b4ed.' . 'a' x 72,
+        FilenameNew  => 'test_5f2a12b7a37e955f02cd994884d4b4ed.' . 'a' x 182,
     },
     {
         Name         => 'FilenameCleanUp() - Long filename no extension - 2 bytes character - Name',
         FilenameOrig => 'ß' x 250,
-        FilenameNew  => 'ß' x 77 . '_7085d519eeab3d2ad3c2ea46b907f96f',
+        FilenameNew  => 'ß' x 93 . '_7085d519eeab3d2ad3c2ea46b907f96f',
     },
     {
         Name         => 'FilenameCleanUp() - Long filename short extension - 2 bytes character - Name',
         FilenameOrig => 'ß' x 250 . '.test',
-        FilenameNew  => 'ß' x 72 . '_dd0aa71f30e5ec4fdfc55e0947ea883c.test',
+        FilenameNew  => 'ß' x 91 . '_dd0aa71f30e5ec4fdfc55e0947ea883c.test',
     },
     {
         Name         => 'FilenameCleanUp() - Short filename long extension - 2 bytes character - Name',
         FilenameOrig => 'test.' . 'ß' x 250,
-        FilenameNew  => 'test_66e18964767c3f2f6a9f1dad7c977aa7.' . 'ß' x 72,
+        FilenameNew  => 'test_66e18964767c3f2f6a9f1dad7c977aa7.' . 'ß' x 91,
     },
     {
         Name         => 'FilenameCleanUp() - Filename ending with period - Name',
@@ -124,52 +124,56 @@ my @Tests = (
         FilenameOrig =>
             'me_to/a+lal123456789012345678901234567890Liebe Grüße aus Straubing123456789012345678901234567890123456789012345678901234567890.xml',
         FilenameNew =>
-            'me_to_a+lal123456789012345678901234567890Liebe Grüße aus Straubing1234567_c5786476990dd136196348ea8d01628b.xml',
+            'me_to_a+lal123456789012345678901234567890Liebe Grüße aus Straubing123456789012345678901234567890123456789012345678901234567890_c5786476990dd136196348ea8d01628b.xml',
     },
     {
         Name => 'FilenameCleanUp() - UTF-8 filename in limit - Name',
         FilenameOrig =>
-            'ąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęśżź.ĘĆ',
+            'ąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóś.aż',
         FilenameNew =>
-            'ąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęśżź.ĘĆ',
+            'ąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóś.aż',
     },
     {
         Name => 'FilenameCleanUp() - UTF-8 filename 3 char over limit - Name',
         FilenameOrig =>
-            'ąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęśżź.ĘĆąĄć',
+            'ąĄćęŃłóśżźąĄćęŃłóśż12.źąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśaż3',
         FilenameNew =>
-            'ąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźą_37a1ea270ec452eeac2038db24e54d4b.ĘĆąĄć',
+            'ąĄćęŃłóśżź_0020daeb65abd8566c9afd8b1c9c1c29.źąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄ',
     },
     {
         Name => 'FilenameCleanUp() - Too long filename UTF-8 - Name',
         FilenameOrig =>
             'ąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżź.bad',
         FilenameNew =>
-            'ąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄć_59a36d4e09b4d7e015d26dc16f2bce7c.bad',
+            'ąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźą_59a36d4e09b4d7e015d26dc16f2bce7c.bad',
     },
     {
         Name => 'FilenameCleanUp() - Too long filename UTF-8 with 30 char ext - Name',
         FilenameOrig =>
             'ąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżź.012345678901234567890123456789',
         FilenameNew =>
-            'ąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃł_44ae3e8ee61809512e22ba1cc49c73f1.012345678901234567890123456789',
+            'ąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóś_44ae3e8ee61809512e22ba1cc49c73f1.012345678901234567890123456789',
     },
     {
         Name => 'FilenameCleanUp() - Too long filename UTF-8 with very long ext - Name',
         FilenameOrig =>
             '01234567890012345678900123456789001234567890.ąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżź',
         FilenameNew =>
-            '01234567890012345678900123456789001234567890_37bd9ded4839178170effa43954f5b75.ąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄ',
+            '0123456789_37bd9ded4839178170effa43954f5b75.ąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóśżźąĄćęŃłóś',
     },
     {
         Name         => 'FilenameCleanUp() - long similar filename 1 - Name',
-        FilenameOrig => '0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę001.txt',
-        FilenameNew  => '0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę012_c70e87af877b541afe6df64abe3f5b6c.txt',
+        FilenameOrig =>
+            '0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę001.txt',
+        FilenameNew  =>
+            '0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0_c70e87af877b541afe6df64abe3f5b6c.txt',
     },
     {
         Name         => 'FilenameCleanUp() - long similar filename 2 - Name',
-        FilenameOrig => '0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę002.txt',
-        FilenameNew  => '0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę012_302561d63d70b2eca7ada1fd3539d9f4.txt',
+        FilenameOrig =>
+            '0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę002.txt',
+        FilenameNew  =>
+            '0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0_302561d63d70b2eca7ada1fd3539d9f4.txt',
     },
     {
         Name         => 'FilenameCleanUp() - md5',

commit 869aaa46bace0397f5bf404d27c353281fc1b78b

Syntax fixed
Fixes: a972be844d55d8cd91f0934cfc0dbafdeb1f059b
Author-Change-Id: IB#1016067

Kernel/System/Main.pm
@@ -258,8 +258,8 @@ sub FilenameCleanUp {
         # result for different original filenames (i.e. in the same upload
         # form or in the same incoming e-mail article) which may cause problems.
         if ( $Param{Filename} ne $FilenameOrig ) {
-            $FileName = $Param{Filename};
-            $FileExt = '';
+            my $FileName = $Param{Filename};
+            my $FileExt = '';
             if ( $Param{Filename} =~ /(.*)\.+([^.]+)$/ ) {
                 $FileName = $1;
                 $FileExt = '.' . $2;