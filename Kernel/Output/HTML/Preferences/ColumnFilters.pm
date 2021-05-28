znuny/Znuny

commit e05ad6a168cbc1128e62a0c19e2d4944cee1a5e4

Column list validation before saving agent preferences
It was noticed in OTRS 3.3, that some agents managed to save their column
filter preferences with duplikcated column names i.e.

    mysql -e 'SELECT * FROM user_preferences where length(preferences_value)>100' otrs
    [...]
    5       UserFilterColumnsEnabled-AgentTicketQueue       ["TicketNumber","Title","State","Lock","Queue","Owner","Age","Changed","CustomerID","Lock","Queue","CustomerID","TicketNumber", [...], "TicketNumber","Age","Title","State","Lock","Queue","CustomerID"]

The whole preference string was over 9kB long (all filled with duplicated
column list as above) and caused apache RAM consumption problems (removing
this record from DB was necessary to avoid out of memory on OTRS
application server). The problem was not reproducible and the cause of
the problem has not been identified (might be some kind of race condition
or web browser bug).

Same problem still exists in Znuny rel-6_0 - to simulate misbehaving
client apply patch

 diff -Nur a/var/httpd/htdocs/js/Core.Agent.Overview.js b/var/httpd/htdocs/js/Core.Agent.Overview.js
 --- a/var/httpd/htdocs/js/Core.Agent.Overview.js<->2021-05-15 16:03:56.586105278 +0200
 +++ b/var/httpd/htdocs/js/Core.Agent.Overview.js>2021-05-25 14:18:25.375359470 +0200
 @@ -83,6 +83,7 @@
                                      // if e.g. the save button is clicked multiple times
                                      if (!$('#' + Core.App.EscapeSelector(FieldID)).length) {
                                          $('<input name="UserFilterColumnsEnabled" type="hidden" />').attr('id', FieldID).val($(this).attr('data-fieldname')).appendTo($ListContainer.closest('div'));
 +                                        $('<input name="UserFilterColumnsEnabled" type="hidden" />').attr('id', FieldID).val($(this).attr('data-fieldname')).appendTo($ListContainer.closest('div'));
                                      }
                                  });

Kernel/Output/HTML/Preferences/ColumnFilters.pm
@@ -1,6 +1,7 @@
 # --
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 # Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+# Copyright (C) 2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -61,10 +62,23 @@ sub Run {
 
         # pref update db
         if ( !$Kernel::OM->Get('Kernel::Config')->Get('DemoSystem') ) {
+
+            my %Seen;
+            my @ColumnsUnique;
+            COLUMN:
+            for my $Column ( @{ $Param{GetParam}->{$Key} } ) {
+
+                # Skip duplicates.
+                next COLUMN if $Seen{$Column};
+                $Seen{$Column} = 1;
+
+                push @ColumnsUnique, $Column;
+            }
+
             $Kernel::OM->Get('Kernel::System::User')->SetPreferences(
                 UserID => $Param{UserData}->{UserID},
                 Key    => $Key . '-' . $FilterAction,
-                Value  => $Kernel::OM->Get('Kernel::System::JSON')->Encode( Data => $Param{GetParam}->{$Key} ),
+                Value  => $Kernel::OM->Get('Kernel::System::JSON')->Encode( Data => \@ColumnsUnique ),
             );
         }
     }

commit db23b73f13426a4dbc4948afcb7ae163ed900bba

Column list saving handler cleanup
Added input data validation and optimized demo
system handling in ColumnFilters.pm.

Fixes: e05ad6a168cbc1128e62a0c19e2d4944cee1a5e4
Related: https://github.com/znuny/Znuny/pull/84
Author-Change-Id: IB#1058144

Kernel/Output/HTML/Preferences/ColumnFilters.pm
@@ -14,6 +14,7 @@ use strict;
 use warnings;
 
 use Kernel::Language qw(Translatable);
+use Kernel::System::VariableCheck qw(:all);
 
 our @ObjectDependencies = (
     'Kernel::System::Web::Request',
@@ -58,27 +59,28 @@ sub Run {
 
     return 1 if !defined $FilterAction;
 
-    for my $Key ( sort keys %{ $Param{GetParam} } ) {
+    if ( !$Kernel::OM->Get('Kernel::Config')->Get('DemoSystem') ) {
+        KEY:
+        for my $Key ( sort keys %{ $Param{GetParam} } ) {
 
-        # pref update db
-        if ( !$Kernel::OM->Get('Kernel::Config')->Get('DemoSystem') ) {
+            # We require non-empty list of column names - ignore antyhing else.
+            next KEY if !( $Param{GetParam}->{$Key} && IsArrayRefWithData( $Param{GetParam}->{$Key} ) );
 
-            my %Seen;
-            my @ColumnsUnique;
-            COLUMN:
-            for my $Column ( @{ $Param{GetParam}->{$Key} } ) {
-
-                # Skip duplicates.
-                next COLUMN if $Seen{$Column};
-                $Seen{$Column} = 1;
-
-                push @ColumnsUnique, $Column;
+            # Remove column name duplicates from list preserving column order.
+            my %SeenColumnNames;
+            my @DeduplicatedColumnNameArray;
+            COLUMN_NAME:
+            for my $ColumnName ( @{ $Param{GetParam}->{$Key} } ) {
+                next COLUMN_NAME if $SeenColumnNames{$ColumnName};
+                $SeenColumnNames{$ColumnName} = 1;
+                push @DeduplicatedColumnNameArray, $ColumnName;
             }
 
+            # Update list in DB.
             $Kernel::OM->Get('Kernel::System::User')->SetPreferences(
                 UserID => $Param{UserData}->{UserID},
                 Key    => $Key . '-' . $FilterAction,
-                Value  => $Kernel::OM->Get('Kernel::System::JSON')->Encode( Data => \@ColumnsUnique ),
+                Value  => $Kernel::OM->Get('Kernel::System::JSON')->Encode( Data => \@DeduplicatedColumnNameArray ),
             );
         }
     }