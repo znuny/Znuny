ce1829e3f4833759d96acc69c03f8c03bfc13d4b
Pawel Boguslawski <pawel.boguslawski@ib.pl>
2021-06-08 09:43:38 +0200

    Fixed Need Value! error in Kernel::System::Stats::Dynamic::TicketList::GetStatTable.
    Editing or executing stat with empty value in dynamic field on selection screen
    throws error like
    
    ERROR: OTRS-CGI-1 Perl: 5.24.1 OS: linux Time: Tue Jun 8 07:15:43 2021
    
     Message: Need Value!
    
     RemoteAddress: 192.168.122.1
     RequestURI: /otrs/index.pl?Action=AgentStatistics;Subaction=Edit;StatID=12
    
     Traceback (2319):.
       Module: Kernel::System::DynamicField::Backend::StatsSearchFieldParameterBuild Line: 2494
       Module: Kernel::System::Stats::Dynamic::TicketList::GetStatTable Line: 954
       Module: Kernel::System::Stats::Dynamic::TicketList::GetStatTablePreview Line: 856
       Module: Kernel::System::Stats::_GenerateDynamicStats Line: 3346
       Module: Kernel::System::Stats::StatsRun Line: 1611
       Module: Kernel::Output::HTML::Statistics::View::PreviewWidget Line: 1209
       Module: Kernel::Modules::AgentStatistics::EditScreen Line: 369
       Module: Kernel::Modules::AgentStatistics::Run Line: 99
       Module: Kernel::System::Web::InterfaceAgent::Run Line: 1141
       Module: ModPerl::ROOT::ModPerl::Registry::opt_otrs_bin_cgi_2dbin_index_2epl::handler Line: 39
       Module: (eval) (v1.99) Line: 207
       Module: ModPerl::RegistryCooker::run (v1.99) Line: 207
       Module: ModPerl::RegistryCooker::default_handler (v1.99) Line: 173
       Module: ModPerl::Registry::handler (v1.99) Line: 32
    
    This fixes the issue.
    
    Author-Change-Id: IB#1081691

--- Kernel/System/Stats/Dynamic/TicketList.pm
+++ Kernel/System/Stats/Dynamic/TicketList.pm
@@ -1,6 +1,7 @@
 # --
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 # Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+# Copyright (C) 2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -949,11 +950,14 @@ sub GetStatTable {
                 next DYNAMICFIELD if !$IsStatsCondition;
 
                 # get new search parameter
-                my $DynamicFieldStatsSearchParameter = $DynamicFieldBackendObject->StatsSearchFieldParameterBuild(
-                    DynamicFieldConfig => $DynamicFieldConfig,
-                    Value              => $Param{Restrictions}->{$ParameterName},
-                    Operator           => $Operator,
-                );
+                my $DynamicFieldStatsSearchParameter;
+                if ($Param{Restrictions}->{$ParameterName}) {
+                    $DynamicFieldStatsSearchParameter = $DynamicFieldBackendObject->StatsSearchFieldParameterBuild(
+                        DynamicFieldConfig => $DynamicFieldConfig,
+                        Value              => $Param{Restrictions}->{$ParameterName},
+                        Operator           => $Operator,
+                    );
+                }
 
                 # add new search parameter
                 if ( !IsHashRefWithData( $DynamicFieldRestrictions{"DynamicField_$FieldName"} ) ) {
