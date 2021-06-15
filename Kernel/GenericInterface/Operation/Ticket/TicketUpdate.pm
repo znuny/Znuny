432c497149da7c94e752057bb1a775f75e5079cb
Paweł Bogusławski <pawel.boguslawski@ib.pl>
2021-06-15 15:41:10 +0200

    Update TicketUpdate.pm

--- Kernel/GenericInterface/Operation/Ticket/TicketUpdate.pm
+++ Kernel/GenericInterface/Operation/Ticket/TicketUpdate.pm
@@ -2,7 +2,6 @@
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 # Copyright (C) 2021 Znuny GmbH, https://znuny.org/
 # Copyright (C) 2021 maxence business consulting GmbH, http://www.maxence.de
-# Copyright (C) 2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
