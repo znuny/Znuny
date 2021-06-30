znuny/Znuny

commit 634d3b5be8652e28caf4c0aa6efcc5d6bd8caf6c

Fixed comparsion operator
Fixes: 81dcbe7512075f8a64feafd621c65f66ba602851
Author-Change-Id: IB#1016490

Kernel/Modules/PictureUpload.pm
@@ -187,10 +187,11 @@ sub Run {
     my @AttachmentMeta = $UploadCacheObject->FormIDGetAllFilesMeta(
         FormID => $FormID
     );
+
     ATTACHMENT:
     for my $Attachment (@AttachmentMeta) {
         next ATTACHMENT if ($File{Filename} ne $Attachment->{Filename})
-            && !(defined($Attachment->{FilenameOrig}) && ($File{Filename} ne $Attachment->{FilenameOrig}));
+            && !(defined($Attachment->{FilenameOrig}) && ($File{Filename} eq $Attachment->{FilenameOrig}));
         $File{Filename} = $Attachment->{Filename};
         $ContentIDNew = $Attachment->{ContentID};
         last ATTACHMENT;

commit c35737a35b8a150e0d712a0d9305d71200098f1e

Unnecessary newline removed
Fixes: 634d3b5be8652e28caf4c0aa6efcc5d6bd8caf6c
Author-Change-Id: IB#1016490

Kernel/Modules/PictureUpload.pm
@@ -187,7 +187,6 @@ sub Run {
     my @AttachmentMeta = $UploadCacheObject->FormIDGetAllFilesMeta(
         FormID => $FormID
     );
-
     ATTACHMENT:
     for my $Attachment (@AttachmentMeta) {
         next ATTACHMENT if ($File{Filename} ne $Attachment->{Filename})