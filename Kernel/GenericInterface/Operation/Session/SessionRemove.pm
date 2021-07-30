88e20edb4ee487b2a770efe808c447126e8892c4
Kevin Janssen <77158891+BuTzZ@users.noreply.github.com>
2021-07-30 16:51:39 +0200

    Added SessionRemove for GenericInterface
    Just give SessionID wich should be removed.

---
+++ Kernel/GenericInterface/Operation/Session/SessionRemove.pm
@@ -0,0 +1,121 @@
+# --
+# Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
+# Copyright (C) 2021-2021 Ease Technologies GmbH, https://ease-technologies.de/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see https://www.gnu.org/licenses/gpl-3.0.txt.
+# --
+
+package Kernel::GenericInterface::Operation::Session::SessionRemove;
+
+use strict;
+use warnings;
+use Data::Dumper;
+
+use Kernel::System::VariableCheck qw(IsStringWithData IsHashRefWithData);
+
+use parent qw(
+    Kernel::GenericInterface::Operation::Common
+    Kernel::GenericInterface::Operation::Session::Common
+);
+
+our $ObjectManagerDisabled = 1;
+
+=head1 NAME
+
+Kernel::GenericInterface::Operation::Session::SessionRemove - GenericInterface Session Remove Operation backend
+
+=head1 PUBLIC INTERFACE
+
+=head2 new()
+
+usually, you want to create an instance of this
+by using Kernel::GenericInterface::Operation->new();
+
+=cut
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # check needed objects
+    for my $Needed (
+        qw(DebuggerObject WebserviceID)
+        )
+    {
+        if ( !$Param{$Needed} ) {
+
+            return {
+                Success      => 0,
+                ErrorMessage => "Got no $Needed!"
+            };
+        }
+
+        $Self->{$Needed} = $Param{$Needed};
+    }
+
+    return $Self;
+}
+
+=head2 Run()
+
+removes a session and returns true (session deleted), false (if
+session can't get deleted)
+
+    $SessionObject->RemoveSessionID(SessionID => '1234567890123456');
+
+=cut
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    if ( !IsHashRefWithData( $Param{Data} ) ) {
+
+        return $Self->ReturnError(
+            ErrorCode    => 'SessionCreate.MissingParameter',
+            ErrorMessage => "SessionCreate: The request is empty!",
+        );
+    }
+
+    for my $Needed (qw( SessionID )) {
+        if ( !$Param{Data}->{$Needed} ) {
+
+            return $Self->ReturnError(
+                ErrorCode    => 'SessionRemove.MissingParameter',
+                ErrorMessage => "SessionRemove: $Needed parameter is missing!",
+            );
+        }
+    }
+    my $Success = $Kernel::OM->Get('Kernel::System::AuthSession')->RemoveSessionID(SessionID => $Param{Data}->{SessionID});
+
+    if ( $Success == 0 ) {
+
+        return $Self->ReturnError(
+            ErrorCode    => 'SessionRemove.Fail',
+            ErrorMessage => "SessionRemove: Could not remove SessionID!",
+        );
+    }
+
+    return {
+        Success => 1,
+        Data    => {
+            Success => $Success,
+        },
+    };
+}
+
+1;
+
+=head1 TERMS AND CONDITIONS
+
+This software is part of the OTRS project (L<https://otrs.org/>).
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see
+the enclosed file COPYING for license information (GPL). If you
+did not receive this file, see L<https://www.gnu.org/licenses/gpl-3.0.txt>.
+
+=cut
