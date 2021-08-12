59cdaa2662b6ff11ea58ddbe596b9c185e58cbcc
Pawel Boguslawski <pawel.boguslawski@ib.pl>
2021-08-12 16:09:16 +0200

    Fixed wrong otrs.Daemon.pl exit code after valid termination
    When otrs.Daemon.pl daemon process receives INT signal to terminate
    (i.e. from "otrs.Daemon.pl stop" command) it exits with code 1 not 0
    as it should.
    
    This mod fixes it.
    
    To reporduce try start with --foreground option introduced in
    https://github.com/znuny/Znuny/pull/399 and then stop.
    
    Related: https://github.com/znuny/Znuny/pull/109#issuecomment-910001106
    Related: https://github.com/znuny/Znuny/pull/399
    Author-Change-Id: IB#1110219

--- bin/otrs.Daemon.pl
+++ bin/otrs.Daemon.pl
@@ -2,6 +2,7 @@
 # --
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 # Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+# Copyright (C) 2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # --
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -356,7 +357,7 @@ sub Start {
     # Remove current log files without content.
     _LogFilesCleanup();
 
-    return 0;
+    return 1;
 }
 
 sub Stop {
