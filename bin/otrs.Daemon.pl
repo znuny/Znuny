85228b690ecb1291e163a07bcfdcbf20b0247ba5
Paweł Bogusławski <pawel.boguslawski@ib.pl>
2021-08-12 16:48:30 +0200

    Update otrs.Daemon.pl

--- bin/otrs.Daemon.pl
+++ bin/otrs.Daemon.pl
@@ -2,7 +2,6 @@
 # --
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 # Copyright (C) 2021 Znuny GmbH, https://znuny.org/
-# Copyright (C) 2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # --
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
