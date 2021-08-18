75d618bb8f268bc8bed46a9e3eed7eb6e612719a
Denny Korsukéwitz <dennykorsukewitz@gmail.com>
2021-08-18 11:15:22 +0200

    Fixed layout bug in _BuildSelectionDataRefCreate.

--- Kernel/Output/HTML/Layout.pm
+++ Kernel/Output/HTML/Layout.pm
@@ -5468,13 +5468,6 @@ sub _BuildSelectionDataRefCreate {
             push @SortKeys, sort { lc $a cmp lc $b } ( values %List );
         }
 
-        # translate value
-        if ( $OptionRef->{Translation} ) {
-            for my $Row ( sort keys %{$DataLocal} ) {
-                $DataLocal->{$Row} = $Self->{LanguageObject}->Translate( $DataLocal->{$Row} );
-            }
-        }
-
         # sort hash (after the translation)
         if ( $OptionRef->{Sort} eq 'NumericKey' ) {
             @SortKeys = sort { $a <=> $b } ( keys %{$DataLocal} );
@@ -5835,8 +5828,16 @@ sub _BuildSelectionDataRefCreate {
         }
     }
 
+    # translate value
+    if ( $OptionRef->{Translation} ) {
+        for my $Row (  @{$DataRef} ) {
+            $Row->{Value} = $Self->{LanguageObject}->Translate( $Row->{Value} );
+        }
+    }
+
     return $DataRef;
 }
+
 
 =head2 _BuildSelectionOutput()
 
