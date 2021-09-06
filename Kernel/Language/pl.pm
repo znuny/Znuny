znuny/Znuny

commit 7a71ffe70116b3663275d8cc28a3b145c8b7038d

Schedules must be defined in UTC not OTRSTimeZone
Znuny cron schedules (i.e. defined in Generic agent jobs or
Daemon::SchedulerCronTaskManager::Task parameters) after
dc8282e00d72b87d1ca39a9559c0e63a0176810b are UTC schedules not OTRSTimeZone
schedules (not elegant but probably better leave this solution now to avoid
daylight savings quirks).

This mod fixes/adds schedule descriptions to explain this.

Fixes: dc8282e00d72b87d1ca39a9559c0e63a0176810b
Author-Change-Id: IB#1110542

Kernel/Language/pl.pm
@@ -1,7 +1,7 @@
 # --
 # Copyright (C) 2003-2010 Tomasz Melissa <janek at rumianek.com>
 # Copyright (C) 2009 Artur Skalski <skal.ar at wp.pl>
-# Copyright (C) 2011-2013 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
+# Copyright (C) 2011-2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # Copyright (C) 2014 Wojciech Myrda <wmyrda at auticon.pl>, http://www.auticon.pl
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 # Copyright (C) 2021 Znuny GmbH, https://znuny.org/
@@ -648,7 +648,7 @@ sub Data {
         'Schedule minutes' => 'Ustal minuty',
         'Schedule hours' => 'Ustal godziny',
         'Schedule days' => 'Ustal dni',
-        'Automatic execution values are in the system timezone.' => 'Wartości automatycznego wykonania są określone dla strefy czasowej systemu.',
+        'Automatic execution values are in the system timezone.' => 'Harmonogram automatycznego wykonywania w UTC.',
         'Currently this generic agent job will not run automatically.' =>
             'Zadanie nie zostanie uruchomione automatycznie.',
         'To enable automatic execution select at least one value from minutes, hours and days!' =>

Kernel/Output/HTML/Templates/Standard/AdminGenericAgent.tt
@@ -193,7 +193,7 @@
                                 </tbody>
                             </table>
                             <p class="FieldExplanation">
-                                [% Translate("Automatic execution values are in the system timezone.") | html %]
+                                [% Translate("Automatic execution schedule in UTC.") | html %]
                             </p>
 [% RenderBlockStart("JobScheduleWarning") %]
                             <p class="Error SpacingTop">

commit d43bffb422381fc9fea8c5d1e6ef93c31bca8900

Polish translation fix
Fixes: 7a71ffe70116b3663275d8cc28a3b145c8b7038d
Author-Change-Id: IB#1110542

Kernel/Language/pl.pm
@@ -648,7 +648,7 @@ sub Data {
         'Schedule minutes' => 'Ustal minuty',
         'Schedule hours' => 'Ustal godziny',
         'Schedule days' => 'Ustal dni',
-        'Automatic execution values are in the system timezone.' => 'Harmonogram automatycznego wykonywania w UTC.',
+        'Automatic execution schedule in UTC.' => 'Harmonogram automatycznego wykonywania w UTC.',
         'Currently this generic agent job will not run automatically.' =>
             'Zadanie nie zostanie uruchomione automatycznie.',
         'To enable automatic execution select at least one value from minutes, hours and days!' =>