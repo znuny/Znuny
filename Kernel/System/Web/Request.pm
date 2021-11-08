znuny/Znuny

commit 62e205052ee01d6a4195d572ba2f1a0ea9573597

Filename cleanup and attachment handling fixes
This mod fixes several problems in filename cleanup
and attachment handling:

* filename celanup logice was messy and contained
  obsolete UTF-* filtering,

* if two different filenames (i.e. attachment names from e-mail
  that differ only with 1 blaclisted char) were cleaned up
  to the same filename, only one of them was displayed in UI,

* mapping attached message/rfc822 message subject to filename
  was not optimal and didn't handle empty subjects well,

* Ajax UI did not support filename cleanups at server side
  on attachment upload,

* a few uninitialized variables generated errors in apache
  error.log,

* long attachment filenames (i.e. >200 chars) were not fully
  displayed in UI (hidden overflow); no file extension (better
  to display it for security reasons) nor link to download
  was displayed in such cases,

Note:
This mod does not resolve problems with upload cache DB backend
because it'll require DB schema change (no column for original
filename in SQL web_upload_cache table available now); uploading
long filenames (i.e 255 chars long) to DB upload cache will
generate problems and attachemnt filename truncation. Use upload
cache FS backend to avoid such surprises.

Related: https://github.com/OTRS/otrs/pull/1181
Related: https://github.com/OTRS/otrs/pull/1959
Author-Change-Id: IB#1016067

Kernel/System/Web/Request.pm
@@ -1,6 +1,7 @@
 # --
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 # Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+# Copyright (C) 2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -246,9 +247,10 @@ gets file upload data.
     );
 
     returns (
-        Filename    => 'abc.txt',
-        ContentType => 'text/plain',
-        Content     => 'Some text',
+        Filename     => 'abc_123.txt',
+        FilenameOrig => 'abc 123.txt',
+        ContentType  => 'text/plain',
+        Content      => 'Some text',
     );
 
 =cut
@@ -260,22 +262,21 @@ sub GetUploadAll {
     my $Upload = $Self->{Query}->upload( $Param{Param} );
     return if !$Upload;
 
+    my $EncodeObject = $Kernel::OM->Get('Kernel::System::Encode');
+
     # get real file name
     my $UploadFilenameOrig = $Self->GetParam( Param => $Param{Param} ) || 'unknown';
 
     my $NewFileName = "$UploadFilenameOrig";    # use "" to get filename of anony. object
-    $Kernel::OM->Get('Kernel::System::Encode')->EncodeInput( \$NewFileName );
+    $EncodeObject->EncodeInput( \$NewFileName );
 
-    # replace all devices like c: or d: and dirs for IE!
-    $NewFileName =~ s/.:\\(.*)/$1/g;
-    $NewFileName =~ s/.*\\(.+?)/$1/g;
+    my $NewFileNameOrig = "$UploadFilenameOrig";    # use "" to get filename of anony. object
+    $EncodeObject->EncodeInput( \$NewFileNameOrig );
 
-    # Remove leading and trailing white space from filename.
-    $Kernel::OM->Get('Kernel::System::CheckItem')->StringClean(
-        StringRef => \$NewFileName,
-        TrimLeft  => 1,
-        TrimRight => 1,
-    );
+#    # Cleanup filename.
+#    $NewFileName = $Kernel::OM->Get('Kernel::System::Main')->FilenameCleanUp(
+#        Filename => $NewFileName,
+#    );
 
     # return a string
     my $Content = '';
@@ -289,10 +290,13 @@ sub GetUploadAll {
         Header   => 'Content-Type',
     );
 
+    $Kernel::OM->Get('Kernel::System::Encode')->EncodeInput( \$UploadFilenameOrig );
+
     return (
-        Filename    => $NewFileName,
-        Content     => $Content,
-        ContentType => $ContentType,
+        Filename     => $NewFileName,
+        FilenameOrig => $NewFileNameOrig,
+        Content      => $Content,
+        ContentType  => $ContentType,
     );
 }
 

Kernel/System/Web/UploadCache/FS.pm
@@ -1,6 +1,7 @@
 # --
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 # Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+# Copyright (C) 2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -101,6 +102,7 @@ sub FormIDAddFile {
     return if !$Self->_FormIDValidate( $Param{FormID} );
 
     $Param{Content} = '' if !defined( $Param{Content} );
+    $Param{FilenameOrig} = '' if !defined( $Param{FilenameOrig} );
 
     # create content id
     my $ContentID   = $Param{ContentID};
@@ -133,38 +135,50 @@ sub FormIDAddFile {
     # get main object
     my $MainObject = $Kernel::OM->Get('Kernel::System::Main');
 
+    $Param{Filename} = $MainObject->FilenameCleanUp(
+        Filename => $Param{Filename},
+    );
+
     # files must readable for creator
     return if !$MainObject->FileWrite(
-        Directory  => $Directory,
-        Filename   => "$Param{Filename}",
-        Content    => \$Param{Content},
-        Mode       => 'binmode',
-        Permission => '640',
-        NoReplace  => 1,
+        Directory       => $Directory,
+        Filename        => "$Param{Filename}",
+        Content         => \$Param{Content},
+        Mode            => 'binmode',
+        Permission      => '640',
+        NoFilenameClean => 1,
     );
     return if !$MainObject->FileWrite(
-        Directory  => $Directory,
-        Filename   => "$Param{Filename}.ContentType",
-        Content    => \$Param{ContentType},
-        Mode       => 'binmode',
-        Permission => '640',
-        NoReplace  => 1,
+        Directory       => $Directory,
+        Filename        => "$Param{Filename}.ContentType",
+        Content         => \$Param{ContentType},
+        Mode            => 'binmode',
+        Permission      => '640',
+        NoFilenameClean => 1,
     );
     return if !$MainObject->FileWrite(
-        Directory  => $Directory,
-        Filename   => "$Param{Filename}.ContentID",
-        Content    => \$ContentID,
-        Mode       => 'binmode',
-        Permission => '640',
-        NoReplace  => 1,
+        Directory       => $Directory,
+        Filename        => "$Param{Filename}.ContentID",
+        Content         => \$ContentID,
+        Mode            => 'binmode',
+        Permission      => '640',
+        NoFilenameClean => 1,
     );
     return if !$MainObject->FileWrite(
-        Directory  => $Directory,
-        Filename   => "$Param{Filename}.Disposition",
-        Content    => \$Disposition,
-        Mode       => 'binmode',
-        Permission => '644',
-        NoReplace  => 1,
+        Directory       => $Directory,
+        Filename        => "$Param{Filename}.Disposition",
+        Content         => \$Disposition,
+        Mode            => 'binmode',
+        Permission      => '644',
+        NoFilenameClean => 1,
+    );
+    return if !$MainObject->FileWrite(
+        Directory       => $Directory,
+        Filename        => "$Param{Filename}.FilenameOrig",
+        Content         => \$Param{FilenameOrig},
+        Mode            => 'utf8',
+        Permission      => '644',
+        NoFilenameClean => 1,
     );
     return 1;
 }
@@ -186,10 +200,13 @@ sub FormIDRemoveFile {
 
     my @Index = @{ $Self->FormIDGetAllFilesMeta(%Param) };
 
-    # finish if files have been already removed by other process
+    # Finish if files have been already removed by other process.
     return if !@Index;
 
-    my $ID   = $Param{FileID} - 1;
+    my $ID = $Param{FileID} - 1;
+
+    return if !defined $Index[$ID];
+
     my %File = %{ $Index[$ID] };
 
     my $Directory = $Self->{TempDir} . '/' . $Param{FormID};
@@ -202,24 +219,29 @@ sub FormIDRemoveFile {
     my $MainObject = $Kernel::OM->Get('Kernel::System::Main');
 
     $MainObject->FileDelete(
-        Directory => $Directory,
-        Filename  => "$File{Filename}",
-        NoReplace => 1,
+        Directory       => $Directory,
+        Filename        => "$File{Filename}",
+        NoFilenameClean => 1,
     );
     $MainObject->FileDelete(
-        Directory => $Directory,
-        Filename  => "$File{Filename}.ContentType",
-        NoReplace => 1,
+        Directory       => $Directory,
+        Filename        => "$File{Filename}.ContentType",
+        NoFilenameClean => 1,
     );
     $MainObject->FileDelete(
-        Directory => $Directory,
-        Filename  => "$File{Filename}.ContentID",
-        NoReplace => 1,
+        Directory       => $Directory,
+        Filename        => "$File{Filename}.ContentID",
+        NoFilenameClean => 1,
     );
     $MainObject->FileDelete(
-        Directory => $Directory,
-        Filename  => "$File{Filename}.Disposition",
-        NoReplace => 1,
+        Directory       => $Directory,
+        Filename        => "$File{Filename}.Disposition",
+        NoFilenameClean => 1,
+    );
+    $MainObject->FileDelete(
+        Directory       => $Directory,
+        Filename        => "$File{Filename}.FilenameOrig",
+        NoFilenameClean => 1,
     );
 
     return 1;
@@ -263,6 +285,7 @@ sub FormIDGetAllFilesData {
         next FILE if $File =~ /\.ContentType$/;
         next FILE if $File =~ /\.ContentID$/;
         next FILE if $File =~ /\.Disposition$/;
+        next FILE if $File =~ /\.FilenameOrig$/;
 
         $Counter++;
         my $FileSize = -s $File;
@@ -276,20 +299,23 @@ sub FormIDGetAllFilesData {
             }
         }
         my $Content = $MainObject->FileRead(
-            Location => $File,
-            Mode     => 'binmode',    # optional - binmode|utf8
+            Location        => $File,
+            Mode            => 'binmode',    # optional - binmode|utf8
+            NoFilenameClean => 1,
         );
         next FILE if !$Content;
 
         my $ContentType = $MainObject->FileRead(
-            Location => "$File.ContentType",
-            Mode     => 'binmode',             # optional - binmode|utf8
+            Location        => "$File.ContentType",
+            Mode            => 'binmode',             # optional - binmode|utf8
+            NoFilenameClean => 1,
         );
         next FILE if !$ContentType;
 
         my $ContentID = $MainObject->FileRead(
-            Location => "$File.ContentID",
-            Mode     => 'binmode',             # optional - binmode|utf8
+            Location        => "$File.ContentID",
+            Mode            => 'binmode',             # optional - binmode|utf8
+            NoFilenameClean => 1,
         );
         next FILE if !$ContentID;
 
@@ -299,23 +325,31 @@ sub FormIDGetAllFilesData {
         }
 
         my $Disposition = $MainObject->FileRead(
-            Location => "$File.Disposition",
-            Mode     => 'binmode',             # optional - binmode|utf8
+            Location        => "$File.Disposition",
+            Mode            => 'binmode',             # optional - binmode|utf8
+            NoFilenameClean => 1,
         );
         next FILE if !$Disposition;
+
+        my $FilenameOrig = $MainObject->FileRead(
+            Location        => "$File.FilenameOrig",
+            Mode            => 'utf8',                # optional - binmode|utf8
+            NoFilenameClean => 1,
+        );
 
         # strip filename
         $File =~ s/^.*\/(.+?)$/$1/;
         push(
             @Data,
             {
-                Content     => ${$Content},
-                ContentID   => ${$ContentID},
-                ContentType => ${$ContentType},
-                Filename    => $File,
-                Filesize    => $FileSize,
-                FileID      => $Counter,
-                Disposition => ${$Disposition},
+                Content      => ${$Content},
+                ContentID    => ${$ContentID},
+                ContentType  => ${$ContentType},
+                Filename     => $File,
+                FilenameOrig => ${$FilenameOrig},
+                Filesize     => $FileSize,
+                FileID       => $Counter,
+                Disposition  => ${$Disposition},
             },
         );
     }
@@ -361,6 +395,7 @@ sub FormIDGetAllFilesMeta {
         next FILE if $File =~ /\.ContentType$/;
         next FILE if $File =~ /\.ContentID$/;
         next FILE if $File =~ /\.Disposition$/;
+        next FILE if $File =~ /\.FilenameOrig$/;
 
         $Counter++;
         my $FileSize = -s $File;
@@ -375,14 +410,16 @@ sub FormIDGetAllFilesMeta {
         }
 
         my $ContentType = $MainObject->FileRead(
-            Location => "$File.ContentType",
-            Mode     => 'binmode',             # optional - binmode|utf8
+            Location        => "$File.ContentType",
+            Mode            => 'binmode',             # optional - binmode|utf8
+            NoFilenameClean => 1,
         );
         next FILE if !$ContentType;
 
         my $ContentID = $MainObject->FileRead(
-            Location => "$File.ContentID",
-            Mode     => 'binmode',             # optional - binmode|utf8
+            Location        => "$File.ContentID",
+            Mode            => 'binmode',             # optional - binmode|utf8
+            NoFilenameClean => 1,
         );
         next FILE if !$ContentID;
 
@@ -392,22 +429,30 @@ sub FormIDGetAllFilesMeta {
         }
 
         my $Disposition = $MainObject->FileRead(
-            Location => "$File.Disposition",
-            Mode     => 'binmode',             # optional - binmode|utf8
+            Location        => "$File.Disposition",
+            Mode            => 'binmode',             # optional - binmode|utf8
+            NoFilenameClean => 1,
         );
         next FILE if !$Disposition;
+
+        my $FilenameOrig = $MainObject->FileRead(
+            Location        => "$File.FilenameOrig",
+            Mode            => 'utf8',                # optional - binmode|utf8
+            NoFilenameClean => 1,
+        );
 
         # strip filename
         $File =~ s/^.*\/(.+?)$/$1/;
         push(
             @Data,
             {
-                ContentID   => ${$ContentID},
-                ContentType => ${$ContentType},
-                Filename    => $File,
-                Filesize    => $FileSize,
-                FileID      => $Counter,
-                Disposition => ${$Disposition},
+                ContentID    => ${$ContentID},
+                ContentType  => ${$ContentType},
+                Filename     => $File,
+                FilenameOrig => ${$FilenameOrig},
+                Filesize     => $FileSize,
+                FileID       => $Counter,
+                Disposition  => ${$Disposition},
             },
         );
     }

Kernel/Modules/AjaxAttachment.pm
@@ -1,6 +1,7 @@
 # --
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 # Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+# Copyright (C) 2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -51,7 +52,6 @@ sub Run {
     $LayoutObject->ChallengeTokenCheck();
 
     if ( $Self->{Subaction} eq 'Upload' ) {
-
         my %UploadStuff = $ParamObject->GetUploadAll(
             Param => 'Files',
         );
@@ -94,7 +94,7 @@ sub Run {
     }
     elsif ( $Self->{Subaction} eq 'Delete' ) {
 
-        my $Return;
+        my $Return = {};
         my $AttachmentFileID = $ParamObject->GetParam( Param => 'FileID' ) || '';
 
         if ( !$AttachmentFileID ) {

Kernel/Modules/PublicCalendar.pm
@@ -1,6 +1,7 @@
 # --
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 # Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+# Copyright (C) 2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -109,7 +110,6 @@ sub Run {
     # prepare the file name
     my $Filename = $Kernel::OM->Get('Kernel::System::Main')->FilenameCleanUp(
         Filename => "$Calendar{CalendarName}.ics",
-        Type     => 'Attachment',
     );
 
     # send iCal response

Kernel/System/EmailParser.pm
@@ -1,6 +1,7 @@
 # --
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 # Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+# Copyright (C) 2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -717,7 +718,6 @@ sub PartsAttachments {
         # cleanup filename
         $PartData{Filename} = $Kernel::OM->Get('Kernel::System::Main')->FilenameCleanUp(
             Filename => $PartData{Filename},
-            Type     => 'Local',
         );
 
         $PartData{ContentDisposition} = $Part->head()->get('Content-Disposition');
@@ -747,23 +747,23 @@ sub PartsAttachments {
     elsif ( $PartData{ContentType} eq 'message/rfc822' ) {
 
         my ($SubjectString) = $Part->as_string() =~ m/^Subject: ([^\n]*(\n[ \t][^\n]*)*)/m;
-        my $Subject = $Self->_DecodeString( String => $SubjectString ) . '.eml';
+        my $Subject = $Self->_DecodeString( String => $SubjectString );
 
-        # cleanup filename
-        $Subject = $Kernel::OM->Get('Kernel::System::Main')->FilenameCleanUp(
-            Filename => $Subject,
-            Type     => 'Local',
-        );
+        if ( defined($Subject) && ( length($Subject) > 0 ) ) {
 
-        if ( $Subject eq '' ) {
-            $Self->{NoFilenamePartCounter}++;
-            $Subject = "Untitled-$Self->{NoFilenamePartCounter}" . '.eml';
+            # Convert message subject to filename if subject is not empty.
+            $PartData{Filename} = $Kernel::OM->Get('Kernel::System::Main')->FilenameCleanUp(
+                Filename => $Subject . '.eml',
+            );
         }
-        $PartData{Filename} = $Subject;
+        else {
+            $Self->{NoFilenamePartCounter}++;
+            $PartData{Filename} = 'file-' . $Self->{NoFilenamePartCounter} . '.eml';
+        }
     }
     else {
         $Self->{NoFilenamePartCounter}++;
-        $PartData{Filename} = "file-$Self->{NoFilenamePartCounter}";
+        $PartData{Filename} = 'file-' . $Self->{NoFilenamePartCounter};
     }
 
     # parse/get Content-Id, Content-Location and Disposition for html email attachments

commit 703d9398e26195644ae2f5cfed9f24ca0f8a36fa

Fixed picture upload with filename cleanup handling
Pictures dragged to HTML editor sometimes were uploaded incorrectly.
This patch fixes it.

Fixes: 62e205052ee01d6a4195d572ba2f1a0ea9573597
Author-Change-Id: IB#1016067

Kernel/System/Web/Request.pm
@@ -248,7 +248,6 @@ gets file upload data.
 
     returns (
         Filename     => 'abc_123.txt',
-        FilenameOrig => 'abc 123.txt',
         ContentType  => 'text/plain',
         Content      => 'Some text',
     );
@@ -262,16 +261,8 @@ sub GetUploadAll {
     my $Upload = $Self->{Query}->upload( $Param{Param} );
     return if !$Upload;
 
-    my $EncodeObject = $Kernel::OM->Get('Kernel::System::Encode');
-
     # get real file name
-    my $UploadFilenameOrig = $Self->GetParam( Param => $Param{Param} ) || 'unknown';
-
-    my $NewFileName = "$UploadFilenameOrig";    # use "" to get filename of anony. object
-    $EncodeObject->EncodeInput( \$NewFileName );
-
-    my $NewFileNameOrig = "$UploadFilenameOrig";    # use "" to get filename of anony. object
-    $EncodeObject->EncodeInput( \$NewFileNameOrig );
+    my $UploadFilename = $Self->GetParam( Param => $Param{Param} ) || 'unknown';
 
     # return a string
     my $Content = '';
@@ -281,17 +272,17 @@ sub GetUploadAll {
     close $Upload;
 
     my $ContentType = $Self->_GetUploadInfo(
-        Filename => $UploadFilenameOrig,
+        Filename => $UploadFilename,
         Header   => 'Content-Type',
     );
 
-    $Kernel::OM->Get('Kernel::System::Encode')->EncodeInput( \$UploadFilenameOrig );
+    $UploadFilename = "${UploadFilename}";
+    $Kernel::OM->Get('Kernel::System::Encode')->EncodeInput( \$UploadFilename );
 
     return (
-        Filename     => $NewFileName,
-        FilenameOrig => $NewFileNameOrig,
-        Content      => $Content,
-        ContentType  => $ContentType,
+        Filename    => $UploadFilename,
+        Content     => $Content,
+        ContentType => $ContentType,
     );
 }
 

Kernel/System/Web/UploadCache/FS.pm
@@ -102,7 +102,6 @@ sub FormIDAddFile {
     return if !$Self->_FormIDValidate( $Param{FormID} );
 
     $Param{Content} = '' if !defined( $Param{Content} );
-    $Param{FilenameOrig} = '' if !defined( $Param{FilenameOrig} );
 
     # create content id
     my $ContentID   = $Param{ContentID};
@@ -135,9 +134,23 @@ sub FormIDAddFile {
     # get main object
     my $MainObject = $Kernel::OM->Get('Kernel::System::Main');
 
-    $Param{Filename} = $MainObject->FilenameCleanUp(
+
+    # Save original filename if cleanup changes filename;
+    # save empty string as original filename otherwise.
+
+    my $FilenameCleanedUp = $MainObject->FilenameCleanUp(
         Filename => $Param{Filename},
     );
+
+    if ( $FilenameCleanedUp ne $Param{Filename} ) {
+        $Param{FilenameOrig} = $Param{Filename};
+        $Param{Filename} = $FilenameCleanedUp;
+    }
+    else {
+        $Param{FilenameOrig} = '';
+    }
+
+    $Param{Filename} = $FilenameCleanedUp;
 
     # files must readable for creator
     return if !$MainObject->FileWrite(