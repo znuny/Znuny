znuny/Znuny

commit 81dcbe7512075f8a64feafd621c65f66ba602851

Fixed inline image corruption when uploading file with the same name
In ticket compose screen, when you upload inline image from file
(say test.png) using image upload button in ckedit and than upload
same file (test.png) as message attachment, than inline image
will became corrupted (only one file test.png will be present in
upload cache folder when using WebUploadCacheModule
= Kernel::System::Web::UploadCache::FS) and after saving/sending such
article - no inline image will be present.

This mod fixes it by generating random filenames for uploaded images.

Fix was not applied to OTRSCE 6 by previous upstream...

https://github.com/OTRS/otrs/pull/1157

...and still exists in Znuny.

Problem with long filenames mentioned by previous upstream in

https://github.com/OTRS/otrs/pull/1157#issuecomment-239636262

may be solved (for FS upload cache backend) by applying sparate
fix IB#1016067 that was submitted in

https://github.com/znuny/Znuny/pull/77

Related: https://github.com/OTRS/otrs/pull/1157
Related: https://github.com/znuny/Znuny/pull/77
Author-Change-Id: IB#1016490

Kernel/Modules/PictureUpload.pm
@@ -1,6 +1,7 @@
 # --
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 # Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+# Copyright (C) 2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -166,49 +167,31 @@ sub Run {
         $File{Content} = $SafetyCheckResult{String};
     }
 
-    # check if name already exists
-    my @AttachmentMeta = $UploadCacheObject->FormIDGetAllFilesMeta(
-        FormID => $FormID,
-    );
-    my $FilenameTmp    = $File{Filename};
-    my $SuffixTmp      = 0;
-    my $UniqueFilename = '';
-    while ( !$UniqueFilename ) {
-        $UniqueFilename = $FilenameTmp;
-        NEWNAME:
-        for my $Attachment ( reverse @AttachmentMeta ) {
-            next NEWNAME if $FilenameTmp ne $Attachment->{Filename};
-
-            # name exists -> change
-            ++$SuffixTmp;
-            if ( $File{Filename} =~ /^(.*)\.(.+?)$/ ) {
-                $FilenameTmp = "$1-$SuffixTmp.$2";
-            }
-            else {
-                $FilenameTmp = "$File{Filename}-$SuffixTmp";
-            }
-            $UniqueFilename = '';
-            last NEWNAME;
-        }
-    }
+    # Protect against file name collisions by adding random prefix.
+    $File{Filename} = 'inline'
+        . $Kernel::OM->Get('Kernel::System::Main')->GenerateRandomString( Length => 16 )
+        . '-'
+        . $File{Filename};
 
     # add uploaded file to upload cache
     $UploadCacheObject->FormIDAddFile(
         FormID      => $FormID,
-        Filename    => $FilenameTmp,
+        Filename    => $File{Filename},
         Content     => $File{Content},
-        ContentType => $File{ContentType} . '; name="' . $FilenameTmp . '"',
+        ContentType => $File{ContentType} . '; name="' . $File{Filename} . '"',
         Disposition => 'inline',
     );
 
-    # get new content id
+    # Get new content id and update filename after possible cleanup in FormIDAddFile().
     my $ContentIDNew = '';
-    @AttachmentMeta = $UploadCacheObject->FormIDGetAllFilesMeta(
+    my @AttachmentMeta = $UploadCacheObject->FormIDGetAllFilesMeta(
         FormID => $FormID
     );
     ATTACHMENT:
     for my $Attachment (@AttachmentMeta) {
-        next ATTACHMENT if $FilenameTmp ne $Attachment->{Filename};
+        next ATTACHMENT if ($File{Filename} ne $Attachment->{Filename})
+            && !(defined($Attachment->{FilenameOrig}) && ($File{Filename} ne $Attachment->{FilenameOrig}));
+        $File{Filename} = $Attachment->{Filename};
         $ContentIDNew = $Attachment->{ContentID};
         last ATTACHMENT;
     }
@@ -224,7 +207,7 @@ sub Run {
     # if ResponseType is JSON, do not return template content but a JSON structure
     if ( $ResponseType eq 'json' ) {
         my %Result = (
-            fileName => $FilenameTmp,
+            fileName => $File{Filename},
             uploaded => 1,
             url      => $URL,
         );

commit 02d49fdb773377ab2950a226cbe97b0b8e647cf6

Filename in ContentType cleaned up
Filename in ContentType on image upload is now cleanded up to be compatible
with https://github.com/znuny/Znuny/pull/77 (both should be applied for
filenames alignment).

Fixes: 81dcbe7512075f8a64feafd621c65f66ba602851
Related: https://github.com/znuny/Znuny/pull/77
Author-Change-Id: IB#1016490

Kernel/Modules/PictureUpload.pm
@@ -173,12 +173,18 @@ sub Run {
         . '-'
         . $File{Filename};
 
+    # Clean up filename for name in ContentType; for FormIDAddFile we pass filename without
+    # cleaning it up (need original filename and will clean it up independently).
+    my $FilenameCleanedUp = $Kernel::OM->Get('Kernel::System::Main')->FilenameCleanUp(
+        Filename => $File{Filename},
+    );
+
     # add uploaded file to upload cache
     $UploadCacheObject->FormIDAddFile(
         FormID      => $FormID,
         Filename    => $File{Filename},
         Content     => $File{Content},
-        ContentType => $File{ContentType} . '; name="' . $File{Filename} . '"',
+        ContentType => $File{ContentType} . '; name="' . $FilenameCleanedUp . '"',
         Disposition => 'inline',
     );
 