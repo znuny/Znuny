znuny/Znuny

commit 9ba980834e8667071aacd2cab13bc9399e9bf36e

Redundant article search index rebuilding fixed
Article search index is rebuild twice:

(1) in ArticleCreate (ArticleSearchIndexBuild called)

and

(2) by FulltextIndexRebuildWorker.pm task called from otrs.Daemon
(because articles are created in article table with default value
in search_index_needs_rebuild column which is 1).

This is waste of resouces and may cause unnecessarry data
fragmentation in search index backends.

This mod resolves this issue by unflagging article for search
index rebuilding after successfull index rebuild.

To be considered for future versions (not included in this mod):
change article.search_index_needs_rebuild default value in DB
schema to 0 to avoid possible races with otrs.Daemon trying to
update article search index just after insert to article table
in ArticleCreate and before ArticleCreate calls
ArticleSearchIndexBuild.

Author-Change-Id: IB#1108194

Kernel/System/Ticket/Article.pm
@@ -1,6 +1,7 @@
 # --
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 # Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+# Copyright (C) 2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -918,7 +919,27 @@ Returns:
 sub ArticleSearchIndexBuild {
     my ( $Self, %Param ) = @_;
 
-    return $Kernel::OM->Get( $Self->{ArticleSearchIndexModule} )->ArticleSearchIndexBuild(%Param);
+    for my $Needed (qw(ArticleID)) {
+        if ( !$Param{$Needed} ) {
+            $Kernel::OM->Get('Kernel::System::Log')->Log(
+                Priority => 'error',
+                Message  => "Need $Needed!"
+            );
+            return;
+        }
+    }
+
+    if ( $Kernel::OM->Get( $Self->{ArticleSearchIndexModule} )->ArticleSearchIndexBuild(%Param) ) {
+
+        # Unset articles search index rebuild flag after successfull rebuild.
+        $Self->ArticleSearchIndexRebuildFlagSet(
+             ArticleIDs => [$Param{ArticleID}],
+             Value      => 0,
+        );
+        return 1;
+    }
+
+    return;
 }
 
 =head2 ArticleSearchIndexDelete()

Kernel/System/Console/Command/Maint/Ticket/FulltextIndexRebuildWorker.pm
@@ -1,6 +1,7 @@
 # --
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 # Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+# Copyright (C) 2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -192,12 +193,6 @@ sub ArticleIndexRebuild {
                     $Kernel::OM->Get('Kernel::System::Log')->Log(
                         Priority => 'error',
                         Message  => "Could not rebuild index for ArticleID '$ArticleID'!"
-                    );
-                }
-                else {
-                    $ArticleObject->ArticleSearchIndexRebuildFlagSet(
-                        ArticleIDs => [$ArticleID],
-                        Value      => 0,
                     );
                 }
             }

commit 5d0bd4f71288f6c53d613a76d99fd784ed217015

Fixed article index build skipping for archived tickets
When Ticket::SearchIndex::IndexArchivedTickets is disabled, article is indexed for
archived tickets. This mod fixes it.

Fixes: 9ba980834e8667071aacd2cab13bc9399e9bf36e
Author-Change-Id: IB#1108194

Kernel/System/Ticket/Article.pm
@@ -919,7 +919,7 @@ Returns:
 sub ArticleSearchIndexBuild {
     my ( $Self, %Param ) = @_;
 
-    for my $Needed (qw(ArticleID)) {
+    for my $Needed (qw(TicketID ArticleID)) {
         if ( !$Param{$Needed} ) {
             $Kernel::OM->Get('Kernel::System::Log')->Log(
                 Priority => 'error',
@@ -927,6 +927,22 @@ sub ArticleSearchIndexBuild {
             );
             return;
         }
+    }
+
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+
+    # Don't build index, unset rebuild flag and return indexing failure if ticket is archived
+    # and archived ticket indexing is disabled.
+    if ( $ConfigObject->Get('Ticket::ArchiveSystem')
+        && !$ConfigObject->Get('Ticket::SearchIndex::IndexArchivedTickets')
+        && $Kernel::OM->Get('Kernel::System::Ticket')->TicketArchiveFlagGet( TicketID => $Param{TicketID} ) ) {
+
+        $Self->ArticleSearchIndexRebuildFlagSet(
+             ArticleIDs => [$Param{ArticleID}],
+             Value      => 0,
+        );
+
+        return;
     }
 
     if ( $Kernel::OM->Get( $Self->{ArticleSearchIndexModule} )->ArticleSearchIndexBuild(%Param) ) {