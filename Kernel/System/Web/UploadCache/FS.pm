znuny/Znuny

commit 6e42dbb7c2243cda896a2588ea8c6701e9352d11

Upload cache fixes
This mod fixes the following upload cache problems:

(1)
If one uploads many files (dropping files test0.bin-test9.bin in one move) races may occur and file id in DOM won't match file ids in web upload dir on server, i.e.:

```
$(".AttachmentListContainer tbody tr").filter(function() { console.log("%s, %d", $(this).find('td.Filename').text(), $(this).find('a').data('file-id'))})
test9.bin, 10
test8.bin, 8
test7.bin, 8
test6.bin, 7
test5.bin, 4
test4.bin, 5
test3.bin, 3
test2.bin, 3
test1.bin, 2
test0.bin, 1
```

This may be fixed with patch (which unblocks updating all file ids on upload not just for one file)...

```
--- a/var/httpd/htdocs/js/Core.UI.js   2021-11-08 18:53:34.088284466 +0100
+++ b/var/httpd/htdocs/js/Core.UI.js 2021-12-16 18:41:14.090199778 +0100
@@ -697,10 +697,6 @@

                                 $TargetObj = $ExistingItemObj.closest('tr');

-                                if ($TargetObj.find('a').data('file-id')) {
-                                    return;
-                                }
-
                                 $TargetObj
                                     .find('.Filetype')
                                     .text(Attachment.ContentType)
```

...but using file ids for identification of uploaded files is not race condition resistant; to provoke race condition apply patch

```
--- a/Kernel/Modules/AjaxAttachment.pm 2021-06-19 21:59:34.379471088 +0200
+++ b/Kernel/Modules/AjaxAttachment.pm       2021-12-16 18:38:28.943925139 +0100
@@ -56,6 +56,10 @@
             Param => 'Files',
         );

+        if ($UploadStuff{Filename} eq 'test01.bin') {
+            sleep(1);
+        }
+
         $UploadCacheObject->FormIDAddFile(
             FormID      => $Self->{FormID},
             Disposition => 'attachment',
@@ -67,6 +71,10 @@
             FormID => $Self->{FormID},
         );

+        if ($UploadStuff{Filename} eq 'test02.bin') {
+            sleep(2);
+        }
+
         my @AttachmentData;
```

Kernel/System/Web/UploadCache/FS.pm
@@ -1,6 +1,7 @@
 # --
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 # Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+# Copyright (C) 2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -172,7 +173,7 @@ sub FormIDAddFile {
 sub FormIDRemoveFile {
     my ( $Self, %Param ) = @_;
 
-    for my $Needed (qw(FormID FileID)) {
+    for my $Needed (qw(FormID Filename)) {
         if ( !$Param{$Needed} ) {
             $Kernel::OM->Get('Kernel::System::Log')->Log(
                 Priority => 'error',
@@ -189,38 +190,43 @@ sub FormIDRemoveFile {
     # finish if files have been already removed by other process
     return if !@Index;
 
-    my $ID   = $Param{FileID} - 1;
-    my %File = %{ $Index[$ID] };
+    # Find and remove file with given filename; return success if
+    # file not found (to avoid error if user double clicks delete icon).
+    for my $File (@Index) {
+        if ($File->{Filename} eq $Param{Filename}) {
+            my $Directory = $Self->{TempDir} . '/' . $Param{FormID};
 
-    my $Directory = $Self->{TempDir} . '/' . $Param{FormID};
+            if ( !-d $Directory ) {
+                return 1;
+            }
 
-    if ( !-d $Directory ) {
-        return 1;
+            # Get main object.
+            my $MainObject = $Kernel::OM->Get('Kernel::System::Main');
+
+            $MainObject->FileDelete(
+                Directory => $Directory,
+                Filename  => "$File{Filename}",
+                NoReplace => 1,
+            );
+            $MainObject->FileDelete(
+                Directory => $Directory,
+                Filename  => "$File{Filename}.ContentType",
+                NoReplace => 1,
+            );
+            $MainObject->FileDelete(
+                Directory => $Directory,
+                Filename  => "$File{Filename}.ContentID",
+                NoReplace => 1,
+            );
+            $MainObject->FileDelete(
+                Directory => $Directory,
+                Filename  => "$File{Filename}.Disposition",
+                NoReplace => 1,
+            );
+
+            last;
+        }
     }
-
-    # get main object
-    my $MainObject = $Kernel::OM->Get('Kernel::System::Main');
-
-    $MainObject->FileDelete(
-        Directory => $Directory,
-        Filename  => "$File{Filename}",
-        NoReplace => 1,
-    );
-    $MainObject->FileDelete(
-        Directory => $Directory,
-        Filename  => "$File{Filename}.ContentType",
-        NoReplace => 1,
-    );
-    $MainObject->FileDelete(
-        Directory => $Directory,
-        Filename  => "$File{Filename}.ContentID",
-        NoReplace => 1,
-    );
-    $MainObject->FileDelete(
-        Directory => $Directory,
-        Filename  => "$File{Filename}.Disposition",
-        NoReplace => 1,
-    );
 
     return 1;
 }
@@ -254,8 +260,6 @@ sub FormIDGetAllFilesData {
         Filter    => "*",
     );
 
-    my $Counter = 0;
-
     FILE:
     for my $File (@List) {
 
@@ -264,7 +268,6 @@ sub FormIDGetAllFilesData {
         next FILE if $File =~ /\.ContentID$/;
         next FILE if $File =~ /\.Disposition$/;
 
-        $Counter++;
         my $FileSize = -s $File;
 
         # human readable file size
@@ -314,7 +317,6 @@ sub FormIDGetAllFilesData {
                 ContentType => ${$ContentType},
                 Filename    => $File,
                 Filesize    => $FileSize,
-                FileID      => $Counter,
                 Disposition => ${$Disposition},
             },
         );
@@ -352,8 +354,6 @@ sub FormIDGetAllFilesMeta {
         Filter    => "*",
     );
 
-    my $Counter = 0;
-
     FILE:
     for my $File (@List) {
 
@@ -362,7 +362,6 @@ sub FormIDGetAllFilesMeta {
         next FILE if $File =~ /\.ContentID$/;
         next FILE if $File =~ /\.Disposition$/;
 
-        $Counter++;
         my $FileSize = -s $File;
 
         # human readable file size
@@ -406,7 +405,6 @@ sub FormIDGetAllFilesMeta {
                 ContentType => ${$ContentType},
                 Filename    => $File,
                 Filesize    => $FileSize,
-                FileID      => $Counter,
                 Disposition => ${$Disposition},
             },
         );

Kernel/Modules/AgentTicketPhoneCommon.pm
@@ -1,6 +1,7 @@
 # --
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 # Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+# Copyright (C) 2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -431,42 +432,6 @@ sub Run {
             );
         }
 
-        # If is an action about attachments
-        my $IsUpload = 0;
-
-        # attachment delete
-        my @AttachmentIDs = map {
-            my ($ID) = $_ =~ m{ \A AttachmentDelete (\d+) \z }xms;
-            $ID ? $ID : ();
-        } $ParamObject->GetParamNames();
-
-        COUNT:
-        for my $Count ( reverse sort @AttachmentIDs ) {
-            my $Delete = $ParamObject->GetParam( Param => "AttachmentDelete$Count" );
-            next COUNT if !$Delete;
-            $Error{AttachmentDelete} = 1;
-            $UploadCacheObject->FormIDRemoveFile(
-                FormID => $Self->{FormID},
-                FileID => $Count,
-            );
-            $IsUpload = 1;
-        }
-
-        # attachment upload
-        if ( $ParamObject->GetParam( Param => 'AttachmentUpload' ) ) {
-            $IsUpload                = 1;
-            %Error                   = ();
-            $Error{AttachmentUpload} = 1;
-            my %UploadStuff = $ParamObject->GetUploadAll(
-                Param => 'FileUpload',
-            );
-            $UploadCacheObject->FormIDAddFile(
-                FormID      => $Self->{FormID},
-                Disposition => 'attachment',
-                %UploadStuff,
-            );
-        }
-
         # Get and validate draft action.
         my $FormDraftAction = $ParamObject->GetParam( Param => 'FormDraftAction' );
         if ( $FormDraftAction && !$Config->{FormDraft} ) {
@@ -525,7 +490,6 @@ sub Run {
                         ErrorMessage => $Kernel::OM->Get('Kernel::Language')
                             ->Translate( "FormDraft name %s is already in use!", $Title ),
                     );
-                    $IsUpload = 1;
                     last DRAFT;
                 }
             }

Kernel/Modules/AjaxAttachment.pm
@@ -1,6 +1,7 @@
 # --
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 # Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+# Copyright (C) 2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -95,18 +96,18 @@ sub Run {
     elsif ( $Self->{Subaction} eq 'Delete' ) {
 
         my $Return;
-        my $AttachmentFileID = $ParamObject->GetParam( Param => 'FileID' ) || '';
+        my $AttachmentFilename = $ParamObject->GetParam( Param => 'Filename' ) || '';
 
-        if ( !$AttachmentFileID ) {
+        if ( !$AttachmentFilename ) {
             $Return->{Message} = $LayoutObject->{LanguageObject}->Translate(
-                'Error: the file could not be deleted properly. Please contact your administrator (missing FileID).'
+                'Error: the file could not be deleted properly. Please contact your administrator (missing Filename).'
             );
         }
         else {
 
             my $DeleteAttachment = $UploadCacheObject->FormIDRemoveFile(
                 FormID => $Self->{FormID},
-                FileID => $AttachmentFileID,
+                Filename => $AttachmentFilename,
             );
 
             if ($DeleteAttachment) {
@@ -128,8 +129,9 @@ sub Run {
 
                 $Return = {
                     Message => 'Success',
-                    Data    => \@AttachmentData,
+                    NumberOfAttachmentsLeft => scalar @AttachmentData,
                 };
+
             }
         }
 

Kernel/System/Web/UploadCache.pm
@@ -1,6 +1,7 @@
 # --
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 # Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+# Copyright (C) 2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -116,7 +117,7 @@ removes a file from a form id
 
     $UploadCacheObject->FormIDRemoveFile(
         FormID => 12345,
-        FileID => 1,
+        FileName => 'document.txt',
     );
 
 =cut
@@ -135,7 +136,7 @@ returns an array with a hash ref of all files for a Form ID
         FormID => 12345,
     );
 
-    Return data of on hash is Content, ContentType, ContentID, Filename, Filesize, FileID;
+    Return data of on hash is Content, ContentType, ContentID, Filename, Filesize;
 
 =cut
 
@@ -155,7 +156,7 @@ Note: returns no content, only meta data.
         FormID => 12345,
     );
 
-    Return data of hash is ContentType, ContentID, Filename, Filesize, FileID;
+    Return data of hash is ContentType, ContentID, Filename, Filesize;
 
 =cut
 

Kernel/System/Web/UploadCache/DB.pm
@@ -1,6 +1,7 @@
 # --
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 # Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+# Copyright (C) 2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -122,7 +123,7 @@ sub FormIDAddFile {
 sub FormIDRemoveFile {
     my ( $Self, %Param ) = @_;
 
-    for my $Needed (qw(FormID FileID)) {
+    for my $Needed (qw(FormID Filename)) {
         if ( !$Param{$Needed} ) {
             $Kernel::OM->Get('Kernel::System::Log')->Log(
                 Priority => 'error',
@@ -137,16 +138,22 @@ sub FormIDRemoveFile {
     # finish if files have been already removed by other process
     return if !@Index;
 
-    my $ID = $Param{FileID} - 1;
-    $Param{Filename} = $Index[$ID]->{Filename};
+    # Find and remove file with given filename; return success if
+    # file not found (to avoid error if user double clicks delete icon).
+    for my $File (@Index) {
+        if ($File->{Filename} eq $Param{Filename}) {
 
-    return if !$Kernel::OM->Get('Kernel::System::DB')->Do(
-        SQL => '
-            DELETE FROM web_upload_cache
-            WHERE form_id = ?
-                AND filename = ?',
-        Bind => [ \$Param{FormID}, \$Param{Filename} ],
-    );
+            return if !$Kernel::OM->Get('Kernel::System::DB')->Do(
+                SQL => '
+                    DELETE FROM web_upload_cache
+                    WHERE form_id = ?
+                        AND filename = ?',
+                Bind => [ \$Param{FormID}, \$Param{Filename} ],
+            );
+
+            last;
+        }
+    }
 
     return 1;
 }
@@ -154,7 +161,6 @@ sub FormIDRemoveFile {
 sub FormIDGetAllFilesData {
     my ( $Self, %Param ) = @_;
 
-    my $Counter = 0;
     my @Data;
     for my $Needed (qw(FormID)) {
         if ( !$Param{$Needed} ) {
@@ -180,7 +186,6 @@ sub FormIDGetAllFilesData {
     );
 
     while ( my @Row = $DBObject->FetchrowArray() ) {
-        $Counter++;
 
         # encode attachment if it's a postgresql backend!!!
         if ( !$DBObject->GetDatabaseFunction('DirectBlob') ) {
@@ -197,7 +202,6 @@ sub FormIDGetAllFilesData {
                 Filename    => $Row[0],
                 Filesize    => $Row[2],
                 Disposition => $Row[5],
-                FileID      => $Counter,
             }
         );
     }
@@ -208,7 +212,6 @@ sub FormIDGetAllFilesData {
 sub FormIDGetAllFilesMeta {
     my ( $Self, %Param ) = @_;
 
-    my $Counter = 0;
     my @Data;
     for my $Needed (qw(FormID)) {
         if ( !$Param{$Needed} ) {
@@ -233,7 +236,6 @@ sub FormIDGetAllFilesMeta {
     );
 
     while ( my @Row = $DBObject->FetchrowArray() ) {
-        $Counter++;
 
         # add the info
         push(
@@ -244,7 +246,6 @@ sub FormIDGetAllFilesMeta {
                 Filename    => $Row[0],
                 Filesize    => $Row[2],
                 Disposition => $Row[4],
-                FileID      => $Counter,
             }
         );
     }

scripts/test/WebUploadCache.t
@@ -1,6 +1,7 @@
 # --
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 # Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+# Copyright (C) 2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -158,7 +159,7 @@ for my $Module (qw(DB FS)) {
         if ( $Module eq 'FS' ) {
             my $Delete = $UploadCacheObject->FormIDRemoveFile(
                 FormID => $InvalidFormID,
-                FileID => 1,
+                Filename => $Filename,
             );
 
             $Self->False(
@@ -169,7 +170,7 @@ for my $Module (qw(DB FS)) {
 
         my $Delete = $UploadCacheObject->FormIDRemoveFile(
             FormID => $FormID,
-            FileID => 1,
+            Filename => $Filename,
         );
         $Self->True(
             $Delete || '',
@@ -274,7 +275,7 @@ for my $Module (qw(DB FS)) {
         }
         my $Delete = $UploadCacheObject->FormIDRemoveFile(
             FormID => $FormID,
-            FileID => 1,
+            Filename => $Filename,
         );
         $Self->True(
             $Delete || '',

var/httpd/htdocs/js/Core.AJAX.js
@@ -1,6 +1,7 @@
 // --
 // Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 // Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+// Copyright (C) 2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 // --
 // This software comes with ABSOLUTELY NO WARRANTY. For details, see
 // the enclosed file COPYING for license information (GPL). If you
@@ -219,7 +220,6 @@ Core.AJAX = (function (TargetNS) {
                 'Filename' : this.Filename,
                 'Filetype' : this.ContentType,
                 'Filesize' : this.Filesize,
-                'FileID'   : this.FileID,
             });
 
             $(AttachmentItem).prependTo($('.AttachmentList tbody')).fadeIn();

var/httpd/htdocs/js/Core.UI.js
@@ -1,6 +1,7 @@
 // --
 // Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 // Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+// Copyright (C) 2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 // --
 // This software comes with ABSOLUTELY NO WARRANTY. For details, see
 // the enclosed file COPYING for license information (GPL). If you
@@ -697,10 +698,6 @@ Core.UI = (function (TargetNS) {
 
                                 $TargetObj = $ExistingItemObj.closest('tr');
 
-                                if ($TargetObj.find('a').data('file-id')) {
-                                    return;
-                                }
-
                                 $TargetObj
                                     .find('.Filetype')
                                     .text(Attachment.ContentType)
@@ -710,8 +707,7 @@ Core.UI = (function (TargetNS) {
                                     .attr('data-file-size', Attachment.Filesize)
                                     .next('td')
                                     .find('a')
-                                    .removeClass('Hidden')
-                                    .data('file-id', Attachment.FileID);
+                                    .removeClass('Hidden');
                             }
                             else {
 
@@ -719,7 +715,6 @@ Core.UI = (function (TargetNS) {
                                     'Filename' : Attachment.Filename,
                                     'Filetype' : Attachment.ContentType,
                                     'Filesize' : Attachment.Filesize,
-                                    'FileID'   : Attachment.FileID,
                                 });
 
                                 $(AttachmentItem).prependTo($ContainerObj.find('.AttachmentList tbody')).fadeIn();
@@ -808,7 +803,7 @@ Core.UI = (function (TargetNS) {
                 Data = {
                     Action: $(this).data('delete-action') ? $(this).data('delete-action') : 'AjaxAttachment',
                     Subaction: 'Delete',
-                    FileID: $(this).data('file-id'),
+                    Filename: $(this).closest('tr').find('td.Filename').text(),
                     FormID: FormID,
                     ObjectID: $(this).data('object-id'),
                     FieldID: $(this).data('field-id'),
@@ -822,12 +817,7 @@ Core.UI = (function (TargetNS) {
 
                         $(this).remove();
 
-                        if (Response.Data && Response.Data.length) {
-
-                            // go through all attachments and update the FileIDs
-                            $.each(Response.Data, function(index, Attachment) {
-                                $AttachmentListContainerObj.find('.AttachmentList td:contains(' + Attachment.Filename + ')').closest('tr').find('a').data('file-id', Attachment.FileID);
-                            });
+                        if (Response.NumberOfAttachmentsLeft && (Response.NumberOfAttachmentsLeft > 0)) {
                             $AttachmentListContainerObj.find('.Busy').fadeOut();
                         }
                         else {

commit ddb65d4f80bc3db7a08de14e6df46dbd8f494351

Syntax fixed
Fixes: 6e42dbb7c2243cda896a2588ea8c6701e9352d11
Author-Change-Id: IB#1113065

Kernel/System/Web/UploadCache/FS.pm
@@ -206,22 +206,22 @@ sub FormIDRemoveFile {
 
             $MainObject->FileDelete(
                 Directory => $Directory,
-                Filename  => "$File{Filename}",
+                Filename  => $File->{Filename},
                 NoReplace => 1,
             );
             $MainObject->FileDelete(
                 Directory => $Directory,
-                Filename  => "$File{Filename}.ContentType",
+                Filename  => $File->{Filename} . '.ContentType',
                 NoReplace => 1,
             );
             $MainObject->FileDelete(
                 Directory => $Directory,
-                Filename  => "$File{Filename}.ContentID",
+                Filename  => $File->{Filename} . '.ContentID',
                 NoReplace => 1,
             );
             $MainObject->FileDelete(
                 Directory => $Directory,
-                Filename  => "$File{Filename}.Disposition",
+                Filename  => $File->{Filename} . '.Disposition',
                 NoReplace => 1,
             );
 