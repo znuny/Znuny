znuny/Znuny

commit 53a7ba6bc678a17b10606f9092d49056b4a0ddbf

Upload cache fixes
Added missing part of 6e42dbb7c2243cda896a2588ea8c6701e9352d11.

Fixes: 6e42dbb7c2243cda896a2588ea8c6701e9352d11
Author-Change-Id: IB#1113065

Kernel/System/Web/Request.pm
@@ -537,10 +537,12 @@ sub SaveFormDraft {
         $FormData{$Param} = $Value;
     }
 
-    # get files from upload cache
-    my @FileData = $Kernel::OM->Get('Kernel::System::Web::UploadCache')->FormIDGetAllFilesData(
+    # Get files from upload cache.
+    my $UploadCacheObject = $Kernel::OM->Get('Kernel::System::Web::UploadCache');
+    my @FileData = $UploadCacheObject->FormIDGetAllFilesData(
         FormID => $MetaParams{FormID},
     );
+    $UploadCacheObject->FormIDRemove( FormID => $MetaParams{FormID} );
 
     # prepare data to add or update draft
     my %FormDraft = (

commit bdfe94e160054404c52273bd0810396e0864994d

Upload cache directory removal undone
Will be moved to separate issue.

Fixes: 53a7ba6bc678a17b10606f9092d49056b4a0ddbf
Author-Change-Id: IB#1113065

Kernel/System/Web/Request.pm
@@ -537,12 +537,10 @@ sub SaveFormDraft {
         $FormData{$Param} = $Value;
     }
 
-    # Get files from upload cache.
-    my $UploadCacheObject = $Kernel::OM->Get('Kernel::System::Web::UploadCache');
-    my @FileData = $UploadCacheObject->FormIDGetAllFilesData(
+    # get files from upload cache
+    my @FileData = $Kernel::OM->Get('Kernel::System::Web::UploadCache')->FormIDGetAllFilesData(
         FormID => $MetaParams{FormID},
     );
-    $UploadCacheObject->FormIDRemove( FormID => $MetaParams{FormID} );
 
     # prepare data to add or update draft
     my %FormDraft = (