znuny/Znuny

commit d37c24d4e0bcd7d596a6f57b5a9284f19763c3ad

Log flooding with less important messages fixed
This mod changes LDAP search limit message priority from
error to debug in CustomerSearchDetail() like in
b574f54d2599956cb18af139fcf5f28d3ccb67fb.

Related: https://github.com/OTRS/otrs/pull/1308
Author-Change-Id: IB#1113507

Kernel/System/CustomerUser/LDAP.pm
@@ -1067,10 +1067,22 @@ sub CustomerSearchDetail {
     );
 
     if ( $ResultSearch->code() ) {
-        $Kernel::OM->Get('Kernel::System::Log')->Log(
-            Priority => 'error',
-            Message  => $ResultSearch->error(),
-        );
+        if ( $ResultSearch->code() == 4 ) {
+
+            # Result code 4 (LDAP_SIZELIMIT_EXCEEDED) is normal if there
+            # are more items in LDAP than search limit defined in OTRS or
+            # in LDAP server. Avoid spamming logs with such errors.
+            $Kernel::OM->Get('Kernel::System::Log')->Log(
+                Priority => 'debug',
+                Message  => 'LDAP size limit exceeded (' . $ResultSearch->error() . ').',
+            );
+        }
+        else {
+            $Kernel::OM->Get('Kernel::System::Log')->Log(
+                Priority => 'error',
+                Message  => 'Search failed! ' . $ResultSearch->error(),
+            );
+        }
     }
 
     my @TmpCustomerUsers;

commit df7dfc556c4b822d818b5f5c724ae3794580115d

Code policy fixes
Disabled TidyAll::Plugin::OTRS::Perl::SyntaxCheck bacause of throwing error
on imported constant use. Changed Net::LDAP to Net::LDAP::Constant like in

https://metacpan.org/pod/Net::LDAP::Control::Paged

Fixes: d37c24d4e0bcd7d596a6f57b5a9284f19763c3ad
Author-Change-Id: IB#1113507

Kernel/System/CustomerUser/LDAP.pm
@@ -10,11 +10,14 @@
 
 package Kernel::System::CustomerUser::LDAP;
 
+# Throws errors when using LDAP_SIZELIMIT_EXCEEDED constant so disabled for now.
+## nofilter(TidyAll::Plugin::OTRS::Perl::SyntaxCheck)
+
 use strict;
 use warnings;
 
 use Net::LDAP;
-use Net::LDAP qw(LDAP_SIZELIMIT_EXCEEDED);
+use Net::LDAP::Constant qw(LDAP_SIZELIMIT_EXCEEDED);
 use Net::LDAP::Util qw(escape_filter_value);
 
 use Kernel::System::VariableCheck qw(:all);