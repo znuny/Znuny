4dfa8641fcb7c3e00c5c71b13bf72adca0fc910f
Kamil Furtek <kfurtek@opgksoftware.com>
2022-02-03 13:16:51 +0100

    Updated jQuery UI from 1.12.1 to 1.13.1.

--- Kernel/Config/Defaults.pm
+++ Kernel/Config/Defaults.pm
@@ -988,7 +988,7 @@ sub LoadDefaults {
         'thirdparty/jquery-3.6.0/jquery.js',
         'thirdparty/jquery-browser-detection/jquery-browser-detection.js',
         'thirdparty/jquery-validate-1.16.0/jquery.validate.js',
-        'thirdparty/jquery-ui-1.12.1/jquery-ui.js',
+        'thirdparty/jquery-ui-1.13.1/jquery-ui.js',
         'thirdparty/jquery-pubsub/pubsub.js',
         'thirdparty/jquery-jstree-3.3.7/jquery.jstree.js',
         'thirdparty/nunjucks-3.0.1/nunjucks.js',
@@ -1025,7 +1025,7 @@ sub LoadDefaults {
     $Self->{'Loader::Agent::CommonJS'}->{'000-Framework'} = [
         'thirdparty/jquery-3.6.0/jquery.js',
         'thirdparty/jquery-browser-detection/jquery-browser-detection.js',
-        'thirdparty/jquery-ui-1.12.1/jquery-ui.js',
+        'thirdparty/jquery-ui-1.13.1/jquery-ui.js',
         'thirdparty/jquery-ui-touch-punch-0.2.3/jquery.ui.touch-punch.js',
         'thirdparty/jquery-validate-1.16.0/jquery.validate.js',
         'thirdparty/jquery-pubsub/pubsub.js',
