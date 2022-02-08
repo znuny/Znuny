znuny/Znuny

commit 7665989363e52c4370a4427cbf01a8709fc71624

Nonexistent and invalid service names handling in postmaster filters fixed
Znuny throws ugly errors like

  Message: No such ServiceID ()!

  otrs.Console.pl: Use of uninitialized value $ServiceData{"ServiceID"} in string ne at /opt/otrs/Kernel/System/PostMaster/NewTicket.pm line 291.

when matching postmaster filter tries to assign nonexistent service name
to ticket using `X-OTRS-Service` setting.

When matching postmaster filter tries to reassign nonexistent service name
to ticket using `X-OTRS-FollowUp-Service` setting, no errors are produced
and reassignment is ignored.

This mod fixes both issues.

It also adds same service name validations for `X-OTRS-FollowUp-Service`
that are applied to `X-OTRS-Service`.

It also fixes incomplete desctiptions of `KeepChildren` parameter which
is used not only to hide child services on lists but rather to treat/not
treat as invalid any valid child services of invalid parent
(i.e. in postmaster filters).

Author-Change-Id: IB#1115177

Kernel/Config/Files/XML/Ticket.xml
@@ -128,7 +128,7 @@
         </Value>
     </Setting>
     <Setting Name="Ticket::Service::KeepChildren" Required="1" Valid="1">
-        <Description Translatable="1">Retains all services in listings even if they are children of invalid elements.</Description>
+        <Description Translatable="1">Enable if you want valid child services of invalid services to be treated as valid. When disabled, any descendant service of invalid service will be treated as invalid regardless of this descendant own validity.</Description>
         <Navigation>Core::Ticket</Navigation>
         <Value>
             <Item ValueType="Checkbox">0</Item>

Kernel/System/PostMaster/FollowUp.pm
@@ -1,6 +1,7 @@
 # --
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 # Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+# Copyright (C) 2022 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -324,22 +325,69 @@ sub Run {
         );
     }
 
-    # set ticket service
+    # Ticket service handling.
+
     if ( $GetParam{'X-OTRS-FollowUp-Service'} ) {
 
-        $TicketObject->TicketServiceSet(
-            Service  => $GetParam{'X-OTRS-FollowUp-Service'},
-            TicketID => $Param{TicketID},
-            UserID   => $Param{InmailUserID},
+        my $ServiceObject = $Kernel::OM->Get('Kernel::System::Service');
+
+        # Check if service exists
+        my $ServiceID = $ServiceObject->ServiceLookup(
+            Name => $GetParam{'X-OTRS-FollowUp-Service'},
         );
 
-        $Self->{CommunicationLogObject}->ObjectLog(
-            ObjectLogType => 'Message',
-            Priority      => 'Debug',
-            Key           => 'Kernel::System::PostMaster::FollowUp',
-            Value =>
-                "Services update via 'X-OTRS-FollowUp-Service'! Service: $GetParam{'X-OTRS-FollowUp-Service'}.",
-        );
+        if ($ServiceID) {
+
+            # If service with given name exists, don't set it if not active.
+
+            # Get all active services.
+            my %ServiceList = $ServiceObject->ServiceList(
+                Valid        => 1,
+                KeepChildren => $ConfigObject->Get('Ticket::Service::KeepChildren') // 0,
+                UserID       => $Param{InmailUserID},
+            );
+
+            if ( !$ServiceList{$ServiceID} ) {
+                $Self->{CommunicationLogObject}->ObjectLog(
+                    ObjectLogType => 'Message',
+                    Priority      => 'Debug',
+                    Key           => 'Kernel::System::PostMaster::FollowUp',
+                    Value =>
+                        "Ticket service won't be updated to '$GetParam{'X-OTRS-FollowUp-Service'}' (service invalid or is a child of invalid service).",
+                );
+                $GetParam{'X-OTRS-FollowUp-Service'} = '';
+            }
+        }
+        else {
+
+            # If service with given name doesn't exist, don't set it.
+
+            $Self->{CommunicationLogObject}->ObjectLog(
+                ObjectLogType => 'Message',
+                Priority      => 'Debug',
+                Key           => 'Kernel::System::PostMaster::FollowUp',
+                Value =>
+                    "Ticket service won't be updated to '$GetParam{'X-OTRS-FollowUp-Service'}' (service does not exist).",
+            );
+
+            $GetParam{'X-OTRS-FollowUp-Service'} = '';
+        }
+
+        if ( $GetParam{'X-OTRS-FollowUp-Service'} ) {
+            $TicketObject->TicketServiceSet(
+                Service  => $GetParam{'X-OTRS-FollowUp-Service'},
+                TicketID => $Param{TicketID},
+                UserID   => $Param{InmailUserID},
+            );
+
+            $Self->{CommunicationLogObject}->ObjectLog(
+                ObjectLogType => 'Message',
+                Priority      => 'Debug',
+                Key           => 'Kernel::System::PostMaster::FollowUp',
+                Value =>
+                    "Ticket service updated via 'X-OTRS-FollowUp-Service' to '$GetParam{'X-OTRS-FollowUp-Service'}'.",
+            );
+        }
     }
 
     # set ticket sla

Kernel/System/PostMaster/NewTicket.pm
@@ -1,6 +1,7 @@
 # --
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 # Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+# Copyright (C) 2022 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -271,30 +272,50 @@ sub Run {
         Value         => "Going to create new ticket.",
     );
 
+    # Ticket service handling.
+
     if ( $GetParam{'X-OTRS-Service'} ) {
         my $ServiceObject = $Kernel::OM->Get('Kernel::System::Service');
 
         # Check if service exists.
-        my %ServiceData = $ServiceObject->ServiceGet(
-            Name   => $GetParam{'X-OTRS-Service'},
-            UserID => $Param{InmailUserID},
+        my $ServiceID = $ServiceObject->ServiceLookup(
+            Name => $GetParam{'X-OTRS-Service'},
         );
 
-        # Get all service list filtering by KeepChildren SysConfig if available.
-        my %ServiceList = $ServiceObject->ServiceList(
-            Valid        => 1,
-            KeepChildren => $ConfigObject->Get('Ticket::Service::KeepChildren') // 0,
-            UserID       => $Param{InmailUserID},
-        );
+        if ($ServiceID) {
 
-        if ( $ServiceData{ServiceID} ne '' && !$ServiceList{ $ServiceData{ServiceID} } ) {
+            # If service with given name exists, don't set it if not active.
+
+            # Get all active services.
+            my %ServiceList = $ServiceObject->ServiceList(
+                Valid        => 1,
+                KeepChildren => $ConfigObject->Get('Ticket::Service::KeepChildren') // 0,
+                UserID       => $Param{InmailUserID},
+            );
+
+            if ( !$ServiceList{$ServiceID} ) {
+                $Self->{CommunicationLogObject}->ObjectLog(
+                    ObjectLogType => 'Message',
+                    Priority      => 'Debug',
+                    Key           => 'Kernel::System::PostMaster::NewTicket',
+                    Value =>
+                        "Ticket service won't be changed to '$GetParam{'X-OTRS-Service'}' (service invalid or is a child of invalid service).",
+                );
+                $GetParam{'X-OTRS-Service'} = '';
+            }
+        }
+        else {
+
+            # If service with given name doesn't exist, don't set it.
+
             $Self->{CommunicationLogObject}->ObjectLog(
                 ObjectLogType => 'Message',
                 Priority      => 'Debug',
                 Key           => 'Kernel::System::PostMaster::NewTicket',
                 Value =>
-                    "Service $GetParam{'X-OTRS-Service'} does not exists or is invalid or is a child of invalid service.",
+                    "Ticket service won't be changed to '$GetParam{'X-OTRS-Service'}' (service does not exist).",
             );
+
             $GetParam{'X-OTRS-Service'} = '';
         }
     }

Kernel/System/Service.pm
@@ -1,6 +1,7 @@
 # --
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 # Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+# Copyright (C) 2022 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -68,6 +69,8 @@ return a hash list of services
 
     my %ServiceList = $ServiceObject->ServiceList(
         Valid  => 0,   # (optional) default 1 (0|1)
+        KeepChildren => 0,   # (optional) default 0 (0|1); when Valid=1 set to 1 if you want
+                             # to include also valid child services of invalid services.
         UserID => 1,
     );
 

commit 821540030d4884db361009de21f7a561e629cc79

Code simplified
Fixes: 7665989363e52c4370a4427cbf01a8709fc71624
Related: https://github.com/znuny/Znuny/pull/197#pullrequestreview-865904449
Author-Change-Id: IB#1115177

Kernel/Config/Files/XML/Ticket.xml
@@ -128,7 +128,7 @@
         </Value>
     </Setting>
     <Setting Name="Ticket::Service::KeepChildren" Required="1" Valid="1">
-        <Description Translatable="1">Enable if you want valid child services of invalid services to be treated as valid. When disabled, any descendant service of invalid service will be treated as invalid regardless of this descendant own validity.</Description>
+        <Description Translatable="1">Retains all services in listings even if they are children of invalid elements.</Description>
         <Navigation>Core::Ticket</Navigation>
         <Value>
             <Item ValueType="Checkbox">0</Item>

Kernel/System/PostMaster/FollowUp.pm
@@ -330,51 +330,31 @@ sub Run {
 
     if ( $GetParam{'X-OTRS-FollowUp-Service'} ) {
 
-        my $ServiceObject = $Kernel::OM->Get('Kernel::System::Service');
-
-        # Check if service exists
-        my $ServiceID = $ServiceObject->ServiceLookup(
-            Name => $GetParam{'X-OTRS-FollowUp-Service'},
+        # Get all valid services.
+        my %ValidServices = reverse $Kernel::OM->Get('Kernel::System::Service')->ServiceList(
+           Valid        => 1,
+           KeepChildren => $ConfigObject->Get('Ticket::Service::KeepChildren') // 0,
+           UserID       => $Param{InmailUserID},
         );
 
-        if ($ServiceID) {
+        if (!$ValidServices{$GetParam{'X-OTRS-FollowUp-Service'}}) {
 
-            # If service with given name exists, don't set it if not active.
-
-            # Get all active services.
-            my %ServiceList = $ServiceObject->ServiceList(
-                Valid        => 1,
-                KeepChildren => $ConfigObject->Get('Ticket::Service::KeepChildren') // 0,
-                UserID       => $Param{InmailUserID},
-            );
-
-            if ( !$ServiceList{$ServiceID} ) {
-                $Self->{CommunicationLogObject}->ObjectLog(
-                    ObjectLogType => 'Message',
-                    Priority      => 'Debug',
-                    Key           => 'Kernel::System::PostMaster::FollowUp',
-                    Value =>
-                        "Ticket service won't be updated to '$GetParam{'X-OTRS-FollowUp-Service'}' (service invalid or is a child of invalid service).",
-                );
-                $GetParam{'X-OTRS-FollowUp-Service'} = '';
-            }
-        }
-        else {
-
-            # If service with given name doesn't exist, don't set it.
+            # If service with given name does not exist or is invalid don't set it if not active.
 
             $Self->{CommunicationLogObject}->ObjectLog(
                 ObjectLogType => 'Message',
                 Priority      => 'Debug',
                 Key           => 'Kernel::System::PostMaster::FollowUp',
                 Value =>
-                    "Ticket service won't be updated to '$GetParam{'X-OTRS-FollowUp-Service'}' (service does not exist).",
+                    "Ticket service won't be updated to '$GetParam{'X-OTRS-FollowUp-Service'}' (does not exist or is invalid or is a child of invalid service).",
             );
 
             $GetParam{'X-OTRS-FollowUp-Service'} = '';
         }
+        else {
 
-        if ( $GetParam{'X-OTRS-FollowUp-Service'} ) {
+            # If service with given name exists, update ticket service.
+
             $TicketObject->TicketServiceSet(
                 Service  => $GetParam{'X-OTRS-FollowUp-Service'},
                 TicketID => $Param{TicketID},

Kernel/System/PostMaster/NewTicket.pm
@@ -275,45 +275,24 @@ sub Run {
     # Ticket service handling.
 
     if ( $GetParam{'X-OTRS-Service'} ) {
-        my $ServiceObject = $Kernel::OM->Get('Kernel::System::Service');
 
-        # Check if service exists.
-        my $ServiceID = $ServiceObject->ServiceLookup(
-            Name => $GetParam{'X-OTRS-Service'},
+        # Get all valid services.
+        my %ValidServices = reverse $Kernel::OM->Get('Kernel::System::Service')->ServiceList(
+           Valid        => 1,
+           KeepChildren => $ConfigObject->Get('Ticket::Service::KeepChildren') // 0,
+           UserID       => $Param{InmailUserID},
         );
 
-        if ($ServiceID) {
+        if (!$ValidServices{$GetParam{'X-OTRS-Service'}}) {
 
-            # If service with given name exists, don't set it if not active.
-
-            # Get all active services.
-            my %ServiceList = $ServiceObject->ServiceList(
-                Valid        => 1,
-                KeepChildren => $ConfigObject->Get('Ticket::Service::KeepChildren') // 0,
-                UserID       => $Param{InmailUserID},
-            );
-
-            if ( !$ServiceList{$ServiceID} ) {
-                $Self->{CommunicationLogObject}->ObjectLog(
-                    ObjectLogType => 'Message',
-                    Priority      => 'Debug',
-                    Key           => 'Kernel::System::PostMaster::NewTicket',
-                    Value =>
-                        "Ticket service won't be changed to '$GetParam{'X-OTRS-Service'}' (service invalid or is a child of invalid service).",
-                );
-                $GetParam{'X-OTRS-Service'} = '';
-            }
-        }
-        else {
-
-            # If service with given name doesn't exist, don't set it.
+            # If service with given name does not exist or is invalid don't set it if not active.
 
             $Self->{CommunicationLogObject}->ObjectLog(
                 ObjectLogType => 'Message',
                 Priority      => 'Debug',
                 Key           => 'Kernel::System::PostMaster::NewTicket',
                 Value =>
-                    "Ticket service won't be changed to '$GetParam{'X-OTRS-Service'}' (service does not exist).",
+                    "Ticket service won't be set to '$GetParam{'X-OTRS-Service'}' (does not exist or is invalid or is a child of invalid service).",
             );
 
             $GetParam{'X-OTRS-Service'} = '';

Kernel/System/Service.pm
@@ -68,10 +68,9 @@ sub new {
 return a hash list of services
 
     my %ServiceList = $ServiceObject->ServiceList(
-        Valid  => 0,   # (optional) default 1 (0|1)
-        KeepChildren => 0,   # (optional) default 0 (0|1); when Valid=1 set to 1 if you want
-                             # to include also valid child services of invalid services.
-        UserID => 1,
+        Valid        => 0,   # (optional) default 1 (0|1)
+        KeepChildren => 0,   # (optional) default 0 (0|1); when Valid=1 set to 1 if you want to include also valid child services of invalid services.
+        UserID       => 1,
     );
 
 =cut