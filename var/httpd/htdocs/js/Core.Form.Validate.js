e7b0e43519c68f4d88e66d381ce7cef9dabf9118
Slawomir Niedbala <sniedbala@dev.opgksoftware.com>
2022-05-27 16:27:19 +0200

    Fixed select fields not being shown in modernized mode after submit with input errors.

--- var/httpd/htdocs/js/Core.Form.Validate.js
+++ var/httpd/htdocs/js/Core.Form.Validate.js
@@ -841,6 +841,9 @@ Core.Form.Validate = (function (TargetNS) {
                 $(this).parents('.WidgetSimple.Collapsed').toggleClass('Collapsed Expanded');
                 // Highlight error fields.
                 TargetNS.HighlightError(this, 'ServerError');
+                // "Re-modernize" fields that are part of a WidgetSimple.
+                // Otherwise those fields won't be shown as modernized after an error.
+                Core.UI.InputFields.Activate($(this).parents('.WidgetSimple'));
             });
 
             // When the dialog closes, focus the first element which had a server error
