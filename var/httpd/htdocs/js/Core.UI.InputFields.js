1e538aabf2b7b79cea0ffdd50e331179fb8f6de4
Denny Korsukéwitz <dk@znuny.com>
2022-08-01 21:34:14 +0200

    Modernized breaking loop.

--- var/httpd/htdocs/js/Core.UI.InputFields.js
+++ var/httpd/htdocs/js/Core.UI.InputFields.js
@@ -1,6 +1,6 @@
 // --
 // Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
-// Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+// Copyright (C) 2021-2022 Znuny GmbH, https://znuny.org/
 // --
 // This software comes with ABSOLUTELY NO WARRANTY. For details, see
 // the enclosed file COPYING for license information (GPL). If you
@@ -527,7 +527,7 @@ Core.UI.InputFields = (function (TargetNS) {
                     if (i === 0) {
                         while (MaxWidth > 0 && OffsetLeft + $SelectionObj.outerWidth() >= MaxWidth) {
 
-                            // break loop if text length is shorter than 3 or only 3 dots exist
+                            // break loop if text length is shorter than 3 or only 3 dots exists
                             if ($TextObj.text().length <= 3 || $TextObj.text() === '...') {
                                 break;
                             }
