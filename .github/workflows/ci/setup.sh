ed0181083188ac41f956d9c2e9204fdd8e78e0d2
Denny Korsukéwitz <dennykorsukewitz@gmail.com>
2022-09-06 17:02:22 +0200

    Added some small improvements to selenium workflow.

--- .github/workflows/ci/setup.sh
+++ .github/workflows/ci/setup.sh
@@ -27,23 +27,8 @@ su -c "bin/otrs.CheckSum.pl -a create" - otrs
 touch /opt/otrs/installed
 
 # prepare Selenium tests
-if [ "$GITHUB_JOB" == "SeleniumTest-MySQL" ]; then
-chmod 777 -R /opt/otrs/scripts/test/sample
-sed -i 's/\(.*\$DIBI\$.*\)/\1                                                 \
-    \$Self->{"TestHTTPHostname"} = "172.18.0.2";                              \
-    \$Self->{"SecureMode"} = 1;                                               \
-    \$Self->{"SeleniumTestsConfig"} = {                                       \
-        remote_server_addr  => "chrome",                                      \
-        port                => "4444",                                        \
-        platform            => "ANY",                                         \
-        browser_name        => "chrome",                                      \
-        extra_capabilities => {                                               \
-            chromeOptions => {                                                \
-                # disable-infobars makes sure window size calculations are ok \
-                args => [ "disable-infobars" ],                               \
-            },                                                                \
-        },                                                                    \
-    },\n/' /opt/otrs/Kernel/Config.pm
+if [[ "$GITHUB_JOB" =~ ^Selenium ]]; then
+    .github/workflows/ci/config-selenium.sh
 fi
 
 if [ "$DB" ]; then
