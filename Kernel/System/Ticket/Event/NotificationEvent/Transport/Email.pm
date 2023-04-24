245bd8fc27462bf7a75aa01c1a23faafbbe5896d
Pawel Boguslawski <pawel.boguslawski@ib.pl>
2023-04-24 09:00:11 +0200

    NotificationEmailDefaultTemplate option added
    This mod introduces new SysConfig parameter
    
    NotificationEmailDefaultTemplate
    
    that allowes one to specify default template (from
    Kernel/Output/HTML/Templates/Standard/NotificationEvent/Email
    directory) to use when sending notification e-mails.
    
    Author-Change-Id: IB#1050091

--- Kernel/System/Ticket/Event/NotificationEvent/Transport/Email.pm
+++ Kernel/System/Ticket/Event/NotificationEvent/Transport/Email.pm
@@ -1,6 +1,7 @@
 # --
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 # Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+# Copyright (C) 2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -125,10 +126,11 @@ sub SendNotification {
 
     if ( $Param{Notification}->{ContentType} && $Param{Notification}->{ContentType} eq 'text/html' ) {
 
-        # Get configured template with fallback to Default.
-        my $EmailTemplate = $Param{Notification}->{Data}->{TransportEmailTemplate}->[0] || 'Default';
+        # Get configured template with fallback to default.
+        my $EmailTemplate = $Param{Notification}->{Data}->{TransportEmailTemplate}->[0]
+            || $ConfigObject->Get('NotificationEmailDefaultTemplate') || 'Default';
 
-        my $Home              = $Kernel::OM->Get('Kernel::Config')->Get('Home');
+        my $Home              = $ConfigObject->Get('Home');
         my $TemplateDir       = "$Home/Kernel/Output/HTML/Templates/Standard/NotificationEvent/Email";
         my $CustomTemplateDir = "$Home/Custom/Kernel/Output/HTML/Templates/Standard/NotificationEvent/Email";
 
@@ -401,7 +403,8 @@ sub TransportSettingsDisplayGet {
         $Param{$Key} = $Param{Data}->{$Key}->[0];
     }
 
-    my $Home              = $Kernel::OM->Get('Kernel::Config')->Get('Home');
+    my $ConfigObject      = $Kernel::OM->Get('Kernel::Config');
+    my $Home              = $ConfigObject->Get('Home');
     my $TemplateDir       = "$Home/Kernel/Output/HTML/Templates/Standard/NotificationEvent/Email";
     my $CustomTemplateDir = "$Home/Custom/Kernel/Output/HTML/Templates/Standard/NotificationEvent/Email";
 
@@ -431,7 +434,7 @@ sub TransportSettingsDisplayGet {
         Data        => \%Templates,
         Name        => 'TransportEmailTemplate',
         Translation => 0,
-        SelectedID  => $Param{Data}->{TransportEmailTemplate} || 'Default',
+        SelectedID  => $Param{Data}->{TransportEmailTemplate} || $ConfigObject->Get('NotificationEmailDefaultTemplate') || 'Default',
         Class       => 'Modernize W50pc',
     );
 
