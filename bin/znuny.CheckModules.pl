9beb2cf36919f7e988bf791b4a5e377d993336f6
Timo Gurr <timo.gurr@gmail.com>
2023-06-05 09:46:21 +0200

    Fixed package category for iCal-Parser on Gentoo.

--- bin/znuny.CheckModules.pl
+++ bin/znuny.CheckModules.pl
@@ -400,7 +400,7 @@ my @NeededModules = (
         Comment   => 'Required for calendar events.',
         InstTypes => {
             aptget => 'libical-parser-perl',
-            emerge => 'perl-core/iCal-Parser',
+            emerge => 'dev-perl/iCal-Parser',
             zypper => 'perl-iCal-Parser',
             ports  => 'devel/p5-iCal-Parser',
         },
