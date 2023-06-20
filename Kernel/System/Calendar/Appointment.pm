znuny/Znuny

commit 4e23a3803cef37afe2e9e9c2f76cb94776533184

Added ExtFQDN parameter for better security
Znuny uses the fully qualified domain name (FQDN SysConfig setting)
for external IDs generation (i.e. Message-ID in e-mail messages,
ContentID for pasted images). If Znuny instance uses internal
domain for FQDN, this internal domain name will leak to third
parties in e-mail message headers which may be not desirable.

This mod introduces new SysConfig option ExtFQDN which allowes
one to define the fully qualified domain name for external IDs
generation (i.e. Message-ID in e-mail messages, Content-ID for
pasted images).

If ExtFQDN is not defined in SysConfig (default), Znuny will
still use FQDN setting for Message-ID and ContentID generation.

Author-Change-Id: IB#1009570

Kernel/Config/Files/XML/Framework.xml
@@ -67,6 +67,13 @@
             <Item ValueType="String" ValueRegex="">yourhost.example.com</Item>
         </Value>
     </Setting>
+    <Setting Name="ExtFQDN" Required="0" Valid="0" ConfigLevel="200">
+        <Description Translatable="1">Defines the fully qualified domain name for external IDs generation (i.e. Message-ID, ContentID).</Description>
+        <Navigation>Core</Navigation>
+        <Value>
+            <Item ValueType="String" ValueRegex="">yourexternaldomain.example.com</Item>
+        </Value>
+    </Setting>
     <Setting Name="SupportDataCollector::HTTPHostname" Required="0" Valid="0" ConfigLevel="200">
         <Description Translatable="1">Defines the HTTP hostname for the support data collection with the public module 'PublicSupportDataCollector' (e.g. used from the OTRS Daemon).</Description>
         <Navigation>Core::SupportDataCollector</Navigation>

Kernel/System/Calendar/Appointment.pm
@@ -1,6 +1,7 @@
 # --
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 # Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+# Copyright (C) 2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -1646,11 +1647,12 @@ sub GetUniqueID {
     $StartTimeStrg =~ s/[-:]//g;
     $StartTimeStrg =~ s/\s/T/;
 
-    # get system FQDN
-    my $FQDN = $Kernel::OM->Get('Kernel::Config')->Get('FQDN');
+    # get system ExtFQDN
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+    my $ExtFQDN = $ConfigObject->Get('ExtFQDN') || $ConfigObject->Get('FQDN');
 
     # return UniqueID
-    return "$StartTimeStrg-$Hash\@$FQDN";
+    return "$StartTimeStrg-$Hash\@$ExtFQDN";
 }
 
 =head2 AppointmentUpcomingGet()

Kernel/System/Email.pm
@@ -1010,9 +1010,10 @@ sub _EncodeMIMEWords {
 sub _MessageIDCreate {
     my ( $Self, %Param ) = @_;
 
-    my $FQDN = $Kernel::OM->Get('Kernel::Config')->Get('FQDN');
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+    my $ExtFQDN = $ConfigObject->Get('ExtFQDN') || $ConfigObject->Get('FQDN');
 
-    return '<' . time() . '.' . rand(999999) . '@' . $FQDN . '>';
+    return '<' . time() . '.' . rand(999999) . '@' . $ExtFQDN . '>';
 }
 
 sub _CreateMimeEntity {

Kernel/System/HTMLUtils.pm
@@ -1247,14 +1247,16 @@ sub EmbeddedImagesExtract {
         return;
     }
 
-    my $FQDN = $Kernel::OM->Get('Kernel::Config')->Get('FQDN');
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+    my $ExtFQDN = $ConfigObject->Get('ExtFQDN') || $ConfigObject->Get('FQDN');
+
     ${ $Param{DocumentRef} } =~ s{(src=")(data:image/)(png|gif|jpg|jpeg|bmp)(;base64,)(.+?)(")}{
 
         my $Base64String = $5;
 
         my $FileName     = 'pasted-' . time() . '-' . int(rand(1000000)) . '.' . $3;
         my $ContentType  = "image/$3; name=\"$FileName\"";
-        my $ContentID    = 'pasted.' . time() . '.' . int(rand(1000000)) . '@' . $FQDN;
+        my $ContentID    = 'pasted.' . time() . '.' . int(rand(1000000)) . '@' . $ExtFQDN;
 
         my $AttachmentData = {
             Content     => decode_base64($Base64String),

Kernel/System/Ticket/Article/Backend/Email.pm
@@ -276,11 +276,13 @@ sub ArticleSend {
         AttachmentsRef => $Param{Attachment},
     );
 
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+
     # create article
     my $Time      = $DateTimeObject->ToEpoch();
     my $Random    = rand 999999;
-    my $FQDN      = $Kernel::OM->Get('Kernel::Config')->Get('FQDN');
-    my $MessageID = "<$Time.$Random\@$FQDN>";
+    my $ExtFQDN   = $ConfigObject->Get('ExtFQDN') || $ConfigObject->Get('FQDN');
+    my $MessageID = "<$Time.$Random\@$ExtFQDN>";
     my $ArticleID = $Self->ArticleCreate(
         %Param,
         MessageID => $MessageID,
@@ -366,13 +368,14 @@ sub ArticleBounce {
         }
     }
 
+    my $ConfigObject   = $Kernel::OM->Get('Kernel::Config');
     my $DateTimeObject = $Kernel::OM->Create('Kernel::System::DateTime');
 
     # create message id
     my $Time         = $DateTimeObject->ToEpoch();
     my $Random       = rand 999999;
-    my $FQDN         = $Kernel::OM->Get('Kernel::Config')->Get('FQDN');
-    my $NewMessageID = "<$Time.$Random.0\@$FQDN>";
+    my $ExtFQDN      = $ConfigObject->Get('ExtFQDN') || $ConfigObject->Get('FQDN');
+    my $NewMessageID = "<$Time.$Random.0\@$ExtFQDN>";
     my $Email        = $Self->ArticlePlain( ArticleID => $Param{ArticleID} );
 
     # check if plain email exists

Kernel/System/Web/UploadCache/DB.pm
@@ -1,6 +1,7 @@
 # --
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 # Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+# Copyright (C) 2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -97,9 +98,10 @@ sub FormIDAddFile {
     if ( !$ContentID && lc $Disposition eq 'inline' ) {
 
         my $Random = rand 999999;
-        my $FQDN   = $Kernel::OM->Get('Kernel::Config')->Get('FQDN');
+        my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+        my $ExtFQDN = $ConfigObject->Get('ExtFQDN') || $ConfigObject->Get('FQDN');
 
-        $ContentID = "$Disposition$Random.$Param{FormID}\@$FQDN";
+        $ContentID = "$Disposition$Random.$Param{FormID}\@$ExtFQDN";
     }
 
     # write attachment to db

Kernel/System/Web/UploadCache/FS.pm
@@ -1,6 +1,7 @@
 # --
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
 # Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+# Copyright (C) 2021 Informatyka Boguslawski sp. z o.o. sp.k., http://www.ib.pl/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -108,9 +109,10 @@ sub FormIDAddFile {
     if ( !$ContentID && lc $Disposition eq 'inline' ) {
 
         my $Random = rand 999999;
-        my $FQDN   = $Kernel::OM->Get('Kernel::Config')->Get('FQDN');
+        my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+        my $ExtFQDN = $ConfigObject->Get('ExtFQDN') || $ConfigObject->Get('FQDN');
 
-        $ContentID = "$Disposition$Random.$Param{FormID}\@$FQDN";
+        $ContentID = "$Disposition$Random.$Param{FormID}\@$ExtFQDN";
     }
 
     # create cache subdirectory if not exist

commit c1337ffbfe8185739278b43a8bf67ac5806d959d

Added ExternalFQDN parameter for better security.  (#73)
* Added ExternalFQDN parameter for better security

Znuny uses the fully qualified domain name (FQDN SysConfig setting)
for external IDs generation (i.e. Message-ID in e-mail messages,
ContentID for pasted images). If Znuny instance uses internal
domain for FQDN, this internal domain name will leak to third
parties in e-mail message headers which may be not desirable.

This mod introduces new SysConfig option ExternalFQDN which allowes
one to define the fully qualified domain name for external IDs
generation (i.e. Message-ID in e-mail messages, Content-ID for
pasted images).

If ExternalFQDN is not defined in SysConfig (default), Znuny will
still use FQDN setting for Message-ID and ContentID generation.

Author-Change-Id: IB#1009570

* Update Email.pm
* Update HTMLUtils.pm
* Update Email.pm
* Update CHANGES.md
* Update Appointment.pm
* Update Email.pm
* Update HTMLUtils.pm
* Update Email.pm
* Update DB.pm
* Update FS.pm

Fixes: 4e23a3803cef37afe2e9e9c2f76cb94776533184
Related: https://github.com/znuny/Znuny/pull/73#pullrequestreview-1100647796
Author-Change-Id: IB#1009570

Kernel/Config/Files/XML/Framework.xml
@@ -67,6 +67,13 @@
             <Item ValueType="String" ValueRegex="">yourhost.example.com</Item>
         </Value>
     </Setting>
+    <Setting Name="ExternalFQDN" Required="0" Valid="0" ConfigLevel="200">
+        <Description Translatable="1">Defines the fully qualified domain name for external IDs generation (i.e. Message-ID, ContentID).</Description>
+        <Navigation>Core</Navigation>
+        <Value>
+            <Item ValueType="String" ValueRegex="">yourexternaldomain.example.com</Item>
+        </Value>
+    </Setting>
     <Setting Name="SupportDataCollector::HTTPHostname" Required="0" Valid="0" ConfigLevel="200">
         <Description Translatable="1">Defines the HTTP hostname for the support data collection with the public module 'PublicSupportDataCollector' (e.g. used from the Znuny Daemon).</Description>
         <Navigation>Core::SupportDataCollector</Navigation>

Kernel/System/Calendar/Appointment.pm
@@ -1749,11 +1749,12 @@ sub GetUniqueID {
     $StartTimeStrg =~ s/[-:]//g;
     $StartTimeStrg =~ s/\s/T/;
 
-    # get system FQDN
-    my $FQDN = $Kernel::OM->Get('Kernel::Config')->Get('FQDN');
+    # get system ExternalFQDN
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+    my $ExternalFQDN = $ConfigObject->Get('ExternalFQDN') || $ConfigObject->Get('FQDN');
 
     # return UniqueID
-    return "$StartTimeStrg-$Hash\@$FQDN";
+    return "$StartTimeStrg-$Hash\@$ExternalFQDN";
 }
 
 =head2 AppointmentUpcomingGet()

Kernel/System/Email.pm
@@ -1010,9 +1010,10 @@ sub _EncodeMIMEWords {
 sub _MessageIDCreate {
     my ( $Self, %Param ) = @_;
 
-    my $FQDN = $Kernel::OM->Get('Kernel::Config')->Get('FQDN');
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+    my $ExternalFQDN = $ConfigObject->Get('ExternalFQDN') || $ConfigObject->Get('FQDN');
 
-    return '<' . time() . '.' . rand(999999) . '@' . $FQDN . '>';
+    return '<' . time() . '.' . rand(999999) . '@' . $ExternalFQDN . '>';
 }
 
 sub _CreateMimeEntity {

Kernel/System/HTMLUtils.pm
@@ -1317,14 +1317,16 @@ sub EmbeddedImagesExtract {
         return;
     }
 
-    my $FQDN = $Kernel::OM->Get('Kernel::Config')->Get('FQDN');
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+    my $ExternalFQDN = $ConfigObject->Get('ExternalFQDN') || $ConfigObject->Get('FQDN');
+
     ${ $Param{DocumentRef} } =~ s{(src=")(data:image/)(png|gif|jpg|jpeg|bmp)(;base64,)(.+?)(")}{
 
         my $Base64String = $5;
 
         my $FileName     = 'pasted-' . time() . '-' . int(rand(1000000)) . '.' . $3;
         my $ContentType  = "image/$3; name=\"$FileName\"";
-        my $ContentID    = 'pasted.' . time() . '.' . int(rand(1000000)) . '@' . $FQDN;
+        my $ContentID    = 'pasted.' . time() . '.' . int(rand(1000000)) . '@' . $ExternalFQDN;
 
         my $AttachmentData = {
             Content     => decode_base64($Base64String),

Kernel/System/Ticket/Article/Backend/Email.pm
@@ -277,11 +277,13 @@ sub ArticleSend {
         AttachmentsRef => $Param{Attachment},
     );
 
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+
     # create article
     my $Time      = $DateTimeObject->ToEpoch();
     my $Random    = rand 999999;
-    my $FQDN      = $Kernel::OM->Get('Kernel::Config')->Get('FQDN');
-    my $MessageID = "<$Time.$Random\@$FQDN>";
+    my $ExternalFQDN   = $ConfigObject->Get('ExternalFQDN') || $ConfigObject->Get('FQDN');
+    my $MessageID = "<$Time.$Random\@$ExternalFQDN>";
     my $ArticleID = $Self->ArticleCreate(
         %Param,
         MessageID => $MessageID,
@@ -367,13 +369,14 @@ sub ArticleBounce {
         }
     }
 
+    my $ConfigObject   = $Kernel::OM->Get('Kernel::Config');
     my $DateTimeObject = $Kernel::OM->Create('Kernel::System::DateTime');
 
     # create message id
     my $Time         = $DateTimeObject->ToEpoch();
     my $Random       = rand 999999;
-    my $FQDN         = $Kernel::OM->Get('Kernel::Config')->Get('FQDN');
-    my $NewMessageID = "<$Time.$Random.0\@$FQDN>";
+    my $ExternalFQDN      = $ConfigObject->Get('ExternalFQDN') || $ConfigObject->Get('FQDN');
+    my $NewMessageID = "<$Time.$Random.0\@$ExternalFQDN>";
     my $Email        = $Self->ArticlePlain( ArticleID => $Param{ArticleID} );
 
     # check if plain email exists

Kernel/System/Web/UploadCache/DB.pm
@@ -98,9 +98,10 @@ sub FormIDAddFile {
     if ( !$ContentID && lc $Disposition eq 'inline' ) {
 
         my $Random = rand 999999;
-        my $FQDN   = $Kernel::OM->Get('Kernel::Config')->Get('FQDN');
+        my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+        my $ExternalFQDN = $ConfigObject->Get('ExternalFQDN') || $ConfigObject->Get('FQDN');
 
-        $ContentID = "$Disposition$Random.$Param{FormID}\@$FQDN";
+        $ContentID = "$Disposition$Random.$Param{FormID}\@$ExternalFQDN";
     }
 
     # write attachment to db

Kernel/System/Web/UploadCache/FS.pm
@@ -112,9 +112,10 @@ sub FormIDAddFile {
     if ( !$ContentID && lc $Disposition eq 'inline' ) {
 
         my $Random = rand 999999;
-        my $FQDN   = $Kernel::OM->Get('Kernel::Config')->Get('FQDN');
+        my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+        my $ExternalFQDN = $ConfigObject->Get('ExternalFQDN') || $ConfigObject->Get('FQDN');
 
-        $ContentID = "$Disposition$Random.$Param{FormID}\@$FQDN";
+        $ContentID = "$Disposition$Random.$Param{FormID}\@$ExternalFQDN";
     }
 
     # create cache subdirectory if not exist