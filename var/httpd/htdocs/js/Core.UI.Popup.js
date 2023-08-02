znuny/Znuny

commit a20eddf94b64dc30ecf92910fb7bc4b16c4cf612

Improved popup profiles and Frontend::RichText::Settings###Width.

Kernel/Config/Files/XML/Framework.xml
@@ -434,7 +434,7 @@
         <Description Translatable="1">Defines the width for the rich text editor component. Enter number (pixels) or percent value (relative).</Description>
         <Navigation>Frontend::Base</Navigation>
         <Value>
-            <Item ValueType="String" ValueRegex="^\d+%?$">620</Item>
+            <Item ValueType="String" ValueRegex="^\d+%?$">75%</Item>
         </Value>
     </Setting>
     <Setting Name="Frontend::RichText::Settings###Height" Required="1" Valid="1">
@@ -477,6 +477,28 @@
         <Navigation>Frontend::Base</Navigation>
         <Value>
             <Item ValueType="String">div[type]{*}; img[*]; col[width]; style[*]{*}; *[id](*)</Item>
+        </Value>
+    </Setting>
+    <Setting Name="Frontend::PopupProfiles###001-Framework" Required="1" Valid="1">
+        <Description Translatable="1">Global settings for all popup profiles.</Description>
+        <Navigation>Frontend::Base</Navigation>
+        <Value>
+            <Hash>
+                <DefaultItem>
+                    <Hash>
+                        <Item></Item>
+                    </Hash>
+                </DefaultItem>
+                <Item Key="Default">
+                    <Hash>
+                        <Item Key="Left">100px</Item>
+                        <Item Key="Top">100px</Item>
+                        <Item Key="Width">75%</Item>
+                        <Item Key="Height">75%</Item>
+                        <Item Key="WindowURLParams">dependent=yes,location=no,menubar=no,resizable=yes,scrollbars=yes,status=no,toolbar=no</Item>
+                    </Hash>
+                </Item>
+            </Hash>
         </Value>
     </Setting>
     <Setting Name="DisableLoginAutocomplete" Required="0" Valid="1">
@@ -3441,6 +3463,22 @@ You can log in via the following URL:
                 <Item Key="Key" Translatable="1">Avatar</Item>
                 <Item Key="Desc" Translatable="1">Change your avatar image.</Item>
                 <Item Key="Block">Avatar</Item>
+                <Item Key="Prio">1000</Item>
+                <Item Key="Active">1</Item>
+            </Hash>
+        </Value>
+    </Setting>
+    <Setting Name="PreferencesGroups###PopupProfile" Required="0" Valid="1">
+        <Description Translatable="1">Defines the global users popup profile.</Description>
+        <Navigation>Frontend::Agent::View::Preferences</Navigation>
+        <Value>
+            <Hash>
+                <Item Key="Module">Kernel::Output::HTML::Preferences::PopupProfile</Item>
+                <Item Key="PreferenceGroup">Miscellaneous</Item>
+                <Item Key="Key">UserPopupProfile</Item>
+                <Item Key="Label" Translatable="1">Popup Profile</Item>
+                <Item Key="Desc" Translatable="1">Change your default popup profile settings.</Item>
+                <Item Key="Block">PopupProfile</Item>
                 <Item Key="Prio">1000</Item>
                 <Item Key="Active">1</Item>
             </Hash>

var/httpd/htdocs/js/Core.UI.Popup.js
@@ -84,7 +84,7 @@ Core.UI.Popup = (function (TargetNS) {
             WindowURLParams: "dependent=yes,location=no,menubar=no,resizable=yes,scrollbars=yes,status=no,toolbar=no",
             Left: 100,
             Top: 100,
-            Width: 1040,
+            Width: 1200,
             Height: 700
         }
     };
@@ -428,6 +428,24 @@ Core.UI.Popup = (function (TargetNS) {
     };
 
     /**
+     * @name UpdatePopupProfile
+     * @memberof Core.UI.Popup
+     * @function
+     * @param {Object} Param        - The object contains PopupProfile data.
+     * @param {String} Param.Left   - x (horizontal) coordinates of the window relative to the screen in pixels.
+     * @param {String} Param.Top    - y (vertical) coordinates of the window relative to the screen in pixels.
+     * @param {String} Param.Width  - Width in pixels.
+     * @param {String} Param.Height - Height in pixels.
+     * @description
+     *      This function updates the popup profile preferences of given user.
+     */
+    TargetNS.UpdatePopupProfile = function (Param) {
+        $.each(Param, function (Key, Value) {
+            Core.Agent.PreferencesUpdate('UserPopupProfile' + Key, Value);
+        });
+    };
+
+    /**
      * @name OpenPopup
      * @memberof Core.UI.Popup
      * @function
@@ -444,6 +462,9 @@ Core.UI.Popup = (function (TargetNS) {
             NewWindow,
             WindowName,
             ConfirmClosePopup = true,
+            Regex,
+            WidthResult,
+            HeightResult,
             PopupFeatures;
 
         // If we are in a mobile environment on opening the popup, open it as an iframe
@@ -491,8 +512,25 @@ Core.UI.Popup = (function (TargetNS) {
 
                 if (WindowMode === 'Popup') {
                     PopupFeatures = PopupProfiles[PopupProfile].WindowURLParams;
+
+                    // get pixel or percent of width and height
+                    // convert to a valid pixel value for window.open
+                    Regex        = new RegExp('(%|px)$');
+                    WidthResult  = PopupProfiles[PopupProfile].Width.toString().match(Regex);
+                    HeightResult = PopupProfiles[PopupProfile].Height.toString().match(Regex);
+
+                    if (PopupProfiles[PopupProfile].Width && WidthResult && WidthResult[0]){
+                        PopupProfiles[PopupProfile].Width = PopupProfiles[PopupProfile].Width.replace(WidthResult[0],'');
+                        PopupProfiles[PopupProfile].Width = PopupProfiles[PopupProfile].Width * window.screen.availWidth / 100;
+                    }
+
+                    if (PopupProfiles[PopupProfile].Height && HeightResult && HeightResult[0]){
+                        PopupProfiles[PopupProfile].Height = PopupProfiles[PopupProfile].Height.replace(HeightResult[0],'');
+                        PopupProfiles[PopupProfile].Height = PopupProfiles[PopupProfile].Height * window.screen.availHeight / 100;
+                    }
+
                     // Get the position of the current screen on browsers which support it (non-IE) and
-                    //  use it to open the popup on the same screen
+                    // use it to open the popup on the same screen
                     PopupFeatures += ',left=' + ((window.screen.left || 0) + PopupProfiles[PopupProfile].Left);
                     PopupFeatures += ',width=' + PopupProfiles[PopupProfile].Width;
 
@@ -688,6 +726,7 @@ Core.UI.Popup = (function (TargetNS) {
     TargetNS.Init = function () {
 
         var PopupURL,
+            UpdatePopupProfileTimeout,
             PopupClose = Core.Config.Get('PopupClose');
 
         if (PopupClose === 'LoadParentURLAndClose') {
@@ -731,6 +770,24 @@ Core.UI.Popup = (function (TargetNS) {
             // add a class to the body element, if this popup is a real popup
             if (window.opener) {
                 $('body').addClass('RealPopup');
+
+                $(window).on('resize', function(){
+
+                    if(UpdatePopupProfileTimeout) {
+                        clearTimeout(UpdatePopupProfileTimeout);
+                    }
+
+                    // update popup profile data after 3 seconds
+                    UpdatePopupProfileTimeout = setTimeout(function() {
+                        var Param = {
+                            Height: $(window).height(),
+                            Width:  $(window).width(),
+                            Left:   window.screenX,
+                            Top:    window.screenY,
+                        };
+                       TargetNS.UpdatePopupProfile(Param);
+                    },1000);
+                });
             }
         }
     };

commit 4299b8d32055972b7c56fbf97d093e6339293120

Fixed Popup profile positioning. (#434)
Popup profile `top`, `left`, `width` and `height` parameter values should be
interpreted by JS as numbers not strings (`left` popup value was contcatenation
not sum of `window.screen.left` and `PopupProfiles[PopupProfile].Left` and was
producing wrong popup positioning).

Popup positioning on dual monitor setup in Firefox was broken because `screenX`
returns position on spanned screen not current screen and according to

https://developer.mozilla.org/en-US/docs/Web/API/Screen/top
https://developer.mozilla.org/en-US/docs/Web/API/Screen/left

`window.screen.top` and `window.screen.left` are non-standard/obsolete
and should be avoided.

This fixes these positioning problems.

There is no point in displaying `Popup Profile` in agent preferences by default
(may be helpful for debugging only) because it's now automatically updated
on popup resize so it's hidden now by default.

Fixes: a20eddf94b64dc30ecf92910fb7bc4b16c4cf612
Related: https://github.com/znuny/Znuny/issues/432
Author-Change-Id: IB#1133822

* Update CHANGES.md
* Update Core.UI.Popup.js

Kernel/Config/Files/XML/Framework.xml
@@ -3468,7 +3468,7 @@ You can log in via the following URL:
             </Hash>
         </Value>
     </Setting>
-    <Setting Name="PreferencesGroups###PopupProfile" Required="0" Valid="1">
+    <Setting Name="PreferencesGroups###PopupProfile" Required="0" Valid="0">
         <Description Translatable="1">Defines the global users popup profile.</Description>
         <Navigation>Frontend::Agent::View::Preferences</Navigation>
         <Value>

var/httpd/htdocs/js/Core.UI.Popup.js
@@ -513,6 +513,12 @@ Core.UI.Popup = (function (TargetNS) {
                 if (WindowMode === 'Popup') {
                     PopupFeatures = PopupProfiles[PopupProfile].WindowURLParams;
 
+                    // Convert strings to numbers to avoid surprises like concatenation instead of sum.
+                    PopupProfiles[PopupProfile].Width = Number(PopupProfiles[PopupProfile].Width);
+                    PopupProfiles[PopupProfile].Height = Number(PopupProfiles[PopupProfile].Height);
+                    PopupProfiles[PopupProfile].Top = Number(PopupProfiles[PopupProfile].Top);
+                    PopupProfiles[PopupProfile].Left = Number(PopupProfiles[PopupProfile].Left);
+
                     // get pixel or percent of width and height
                     // convert to a valid pixel value for window.open
                     Regex        = new RegExp('(%|px)$');
@@ -531,7 +537,7 @@ Core.UI.Popup = (function (TargetNS) {
 
                     // Get the position of the current screen on browsers which support it (non-IE) and
                     // use it to open the popup on the same screen
-                    PopupFeatures += ',left=' + ((window.screen.left || 0) + PopupProfiles[PopupProfile].Left);
+                    PopupFeatures += ',left=' + PopupProfiles[PopupProfile].Left;
                     PopupFeatures += ',width=' + PopupProfiles[PopupProfile].Width;
 
                     // Bug#11205 (http://bugs.otrs.org/show_bug.cgi?id=11205)
@@ -543,11 +549,11 @@ Core.UI.Popup = (function (TargetNS) {
                     if (window.screen.availHeight < PopupProfiles[PopupProfile].Height + PopupProfiles[PopupProfile].Top) {
                         PopupFeatures += ',height=' + (window.screen.availHeight - PopupProfiles[PopupProfile].Top - 20);
                         // Adjust top position to have the same distance between top and bottom line.
-                        PopupFeatures += ',top=' + ((window.screen.top || 0) + (PopupProfiles[PopupProfile].Top / 2));
+                        PopupFeatures += ',top=' + (PopupProfiles[PopupProfile].Top / 2);
                     }
                     else {
                         PopupFeatures += ',height=' + PopupProfiles[PopupProfile].Height;
-                        PopupFeatures += ',top=' + ((window.screen.top || 0) + PopupProfiles[PopupProfile].Top);
+                        PopupFeatures += ',top=' + PopupProfiles[PopupProfile].Top;
                     }
 
                     NewWindow = window.open(URL, WindowName, PopupFeatures);