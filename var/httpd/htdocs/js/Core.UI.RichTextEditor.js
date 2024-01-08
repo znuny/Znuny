14d3c74ba340f10c2e480f30051ab32638c9cb9e
Paweł Bogusławski <pawel.boguslawski@ib.pl>
2024-01-08 11:14:11 +0100

    Fixed rich text editor's field height problems by disabling CKEditor's autogrow plugin.

--- var/httpd/htdocs/js/Core.UI.RichTextEditor.js
+++ var/httpd/htdocs/js/Core.UI.RichTextEditor.js
@@ -307,7 +307,7 @@ Core.UI.RichTextEditor = (function (TargetNS) {
 
         // set default editor config, but allow custom config for other types for editors
         /*eslint-disable camelcase */
-        RemovedCKEditorPlugins = 'devtools,image,flash,mathjax,embed,exportpdf,sourcedialog,bbcode,divarea,elementspath,stylesheetparser';
+        RemovedCKEditorPlugins = 'devtools,image,flash,mathjax,embed,exportpdf,sourcedialog,bbcode,divarea,elementspath,stylesheetparser,autogrow';
         if (!CheckFormID($EditorArea).length) {
             RemovedCKEditorPlugins += ',uploadimage';
         }
@@ -320,7 +320,6 @@ Core.UI.RichTextEditor = (function (TargetNS) {
             width:                     Core.Config.Get('RichText.Width', 620),
             resize_minWidth:           Core.Config.Get('RichText.Width', 620),
             height:                    Core.Config.Get('RichText.Height', 320),
-            autoGrow_minHeight:        Core.Config.Get('RichText.Height', 320),
             removePlugins:             RemovedCKEditorPlugins,
             forcePasteAsPlainText:     false,
             format_tags:               Core.Config.Get('RichText.FormatTags', 'p;h1;h2;h3;h4;h5;h6;pre'),
