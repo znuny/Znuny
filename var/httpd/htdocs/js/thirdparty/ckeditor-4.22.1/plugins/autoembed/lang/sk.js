60b188dfd78ddc778ba46cf7eb116a47ed2a8513
Jens Pfeifer <jp@znuny.com>
2024-01-18 12:48:51 +0000

    Exchanged CKEditor 4.22.1 source version with minified version.

--- var/httpd/htdocs/js/thirdparty/ckeditor-4.22.1/plugins/autoembed/lang/sk.js
+++
@@ -1,8 +0,0 @@
-﻿/*
- Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
- CKEditor 4 LTS ("Long Term Support") is available under the terms of the Extended Support Model.
- */
-CKEDITOR.plugins.setLang( 'autoembed', 'sk', {
-	embeddingInProgress: 'Snažím sa vložiť skopírovanú URL...',
-	embeddingFailed: 'Túto URL nebolo možné automaticky vložiť.'
-} );
